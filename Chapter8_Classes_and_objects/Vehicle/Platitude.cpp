ae1246b3163b2a0fbb4fbc21d9e8b804ff6bec44
Ceiling_roof <ted10014@hotmail.com>
2022-05-01 14:02:58 +0800

    Committed 2022.05/01

--- Chapter8_Classes_and_objects/Vehicle/Platitude.cpp
+++ Chapter8_Classes_and_objects/Vehicle/Platitude.cpp
@@ -11,5 +11,27 @@ public:
 };
 
 int main() {
+	Vehicle minivan; // create a Vehicle object.
+	Vehicle sportscar; // Create another object.
 
+	int range1, range2;
+
+	// Assign values to fields in minivan.
+	minivan.passenger = 7;
+	minivan.fuelcap = 16;
+	minivan.mpg = 21;
+
+	// Assign values to fields in sportscar. 
+	sportscar.passenger = 2;
+	sportscar.fuelcap = 14;
+	sportscar.mpg = 12;
+
+	// Compute the ranges assuming a full tank of gas.
+	range1 = minivan.fuelcap * minivan.mpg;
+	range2 = sportscar.fuelcap * sportscar.mpg;
+
+	cout << "Minivan can carry " << minivan.passenger << " with a range of " << range1 << "\n";
+	cout << "Sportscar can carry " << sportscar.passenger << " with a range of " << range2 << "\n";
+
+	return 0;
 }
