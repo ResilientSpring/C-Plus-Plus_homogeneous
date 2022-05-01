b388e735db18a16f30e79bdc36d4fc9dbd085c1a
Ceiling_roof <ted10014@hotmail.com>
2022-05-01 13:56:28 +0800

    Committed 2022/05/01

---
+++ Chapter8_Classes_and_objects/Vehicle/Platitude.cpp
@@ -0,0 +1,15 @@
+// This program creates two Vehicle objects
+#include <iostream>
+using namespace std;
+
+// Declare the Vehicle class.
+class Vehicle {
+public:
+	int passenger;
+	int fuelcap;
+	int mpg;
+};
+
+int main() {
+
+}
