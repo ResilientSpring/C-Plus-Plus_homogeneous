9d2cfdc407eb5e86d4aab246fe0f9712dc963939
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-01 18:06:49 +0800

    Committed 2022/05/01

--- Chapter8_Classes_and_objects/Project8-1_Creating_a_Help_Class/Close-up.cpp
+++ Chapter8_Classes_and_objects/Project8-1_Creating_a_Help_Class/Close-up.cpp
@@ -65,6 +65,51 @@ void Help::helpOn(char what) {
 	cout << "\n";
 }
 
-int main() {
+// Show the help menu
+void Help::showMenu() {
+	cout << "Help on: \n";
+	cout << " 1. if\n";
+	cout << " 2. switch \n";
+	cout << " 3. for \n";
+	cout << " 4. while\n";
+	cout << " 5. do-while\n";
+	cout << " 6. break \n";
+	cout << " 7. continue \n";
+	cout << " 8. goto \n";
+	cout << "Choose one (q to quit): ";
+}
 
+// Return true if a selection is valid.
+bool Help::isValid(char ch)
+{
+	if (ch < '1' || ch > '8' && ch != 'q')
+	{
+		return false;
+	} else 
+		return true;
+}
+
+int main() {
+	char choice;
+	Help help_object;  // create an instance of the Help
+
+	// Use the Help object to display information.
+	for (; ; )
+	{
+		do {
+			help_object.showMenu();
+			cin >> choice;
+		} while (!help_object.isValid(choice));
+
+		if (choice == 'q')
+		{
+			break;
+		}
+
+		cout << "\n";
+
+		help_object.helpOn(choice);
+	}
+
+	return 0;
 }
