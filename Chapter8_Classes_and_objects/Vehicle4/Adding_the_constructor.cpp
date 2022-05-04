03788aa28a19adb8844852a0665d98fad74ba037
Ceiling_roof <ted10014@hotmail.com>
2022-05-04 11:36:14 +0800

    Committed 2022/05/04

--- Chapter8_Classes_and_objects/Vehicle4/Adding_the_constructor.cpp
+++ Chapter8_Classes_and_objects/Vehicle4/Adding_the_constructor.cpp
@@ -2,6 +2,40 @@
 #include <iostream>
 using namespace std;
 
+// Declare the Vehicle class
+class Vehicle {
+public:
+	int passengers;      // number of passengers.
+	int fuelcap;         // fuel capacity in gallons. 
+	int mpg;             // fuel consumption in miles per gallon
+
+	// This is a constructor for Vehicle.
+	Vehicle(int p, int f, int m) {
+		passengers = p;
+		fuelcap = f;
+		mpg = m;
+	}
+
+	int range(){
+		return mpg * fuelcap;
+	}
+
+};
+
 int main() {
+
+	Vehicle minivan(7, 16, 21);
+	Vehicle sportscar = Vehicle(2, 14, 12);
+
+	int range1, range2;
+
+	// Compute the ranges assuming a full tank of gas.
+	range1 = minivan.range();
+	range2 = sportscar.range();
+
+	cout << "Minivan can carry " << minivan.passengers << " with a range of " << range1 << "\n";
+	cout << "Sportscar can carry " << sportscar.passengers << " with a range of " << range2 << "\n";
+
+	return 0;
 
 }
