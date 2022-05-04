34961a102ffa287242fe446a461d256e8abe2353
Ceiling_roof <ted10014@hotmail.com>
2022-05-04 12:33:47 +0800

    Committed 2022/05/04

---
+++ Chapter5 - Functions/Creating_a_function/Prototype.cpp
@@ -0,0 +1,14 @@
+// This program contains two functions: main() and myfunc().
+#include <iostream>
+using namespace std;
+
+void myfunc();  // This is myfunc()'s prototype.
+
+int main() {
+
+}
+
+// This is the function's definition.
+void myfunc() {
+	cout << "Inside myfunc()";
+}
