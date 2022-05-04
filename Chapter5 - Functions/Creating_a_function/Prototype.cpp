01a8e12b96f3bfd36e49f99e9de98b7e9866e980
Ceiling_roof <ted10014@hotmail.com>
2022-05-04 12:36:28 +0800

    Committed 2022/05/04

--- Chapter5 - Functions/Creating_a_function/Prototype.cpp
+++ Chapter5 - Functions/Creating_a_function/Prototype.cpp
@@ -5,10 +5,16 @@ using namespace std;
 void myfunc();  // This is myfunc()'s prototype.
 
 int main() {
+	cout << "In main()\n";
 
+	myfunc();  // Call myfunc()
+
+	cout << "Back in main()\n";
+
+	return 0;
 }
 
 // This is the function's definition.
 void myfunc() {
-	cout << "Inside myfunc()";
+	cout << "Inside myfunc()\n";
 }
