87ce3f3a872d6e648269a8d335b54edcaf43b3c5
Ceiling_roof <ted10014@hotmail.com>
2022-05-06 12:30:19 +0800

    Committed 2022/05/06

---
+++ Chapter5 - Functions/Function_prototypes/Discreet.cpp
@@ -0,0 +1,15 @@
+/* 
+
+This program uses a function prototype to enforce strong type checking
+ 
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+}
