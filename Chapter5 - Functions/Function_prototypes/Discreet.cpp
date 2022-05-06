affb0c51db6c5d69bb48ec896398e2608e217d26
Ceiling_roof <ted10014@hotmail.com>
2022-05-06 12:32:58 +0800

    Committed 2022/05/06

--- Chapter5 - Functions/Function_prototypes/Discreet.cpp
+++ Chapter5 - Functions/Function_prototypes/Discreet.cpp
@@ -10,6 +10,18 @@ This program uses a function prototype to enforce strong type checking
 #include <iomanip>
 using namespace std;
 
-int main() {
+void sqr_it(int* i);
 
+int main() {
+	int x;
+
+	x = 10;
+
+	sqr_it(x); // Error!
+
+	return 0;
+}
+
+void sqr_it(int* i) {
+	*i = *i * *i;
 }
