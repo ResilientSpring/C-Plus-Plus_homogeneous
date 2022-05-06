63b3a3b3839efe9c0013924eede0e860c10abd89
Ceiling_roof <ted10014@hotmail.com>
2022-05-07 01:15:37 +0800

    Committed 2022/05/07

--- Modern C for Absolute Beginners/static_Global_Names/Source.cpp
+++ Modern C for Absolute Beginners/static_Global_Names/Source.cpp
@@ -7,6 +7,13 @@ using namespace std;
 // global scope
 static int globalx = 123;
 
+void globalfn(void) {
+	printf("The value of a global var is:%d\n", globalx);
+}
+
 int main() {
-	
+	// local scope
+	int localx = 456;
+	globalfn();
+	printf("The value of a local var is: %d\n", localx);
 }
