40acafede832be875430a5dfe96a64a95ab437ea
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-10 00:10:25 +0800

    Committed 2022/05/10

--- Chapter7 - More Data/static_local_variable/Civic_duty.cpp
+++ Chapter7 - More Data/static_local_variable/Civic_duty.cpp
@@ -1,1 +1,13 @@
 // Compute a running average of numbers entered by the user.
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+}
+
+int running_average(int i) {
+
+}
