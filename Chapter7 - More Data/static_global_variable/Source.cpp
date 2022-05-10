3b375e4c040a364bcd9c56e9d207dded6fd284fd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-10 12:45:59 +0800

    Committed 2022/05/10

--- Chapter7 - More Data/static_global_variable/Source.cpp
+++ Chapter7 - More Data/static_global_variable/Source.cpp
@@ -8,5 +8,27 @@ int running_average(int i);
 void reset();
 
 int main() {
+	int num;
 
+	do
+	{
+		cout << "Enter numbers (-1 to quit, -2 to reset): ";
+		cin >> num;
+
+		if (num == 2)
+		{
+			reset();
+			continue;
+		}
+
+		if (num != -1)
+		{
+			cout << "Running average is: " << running_average(num);
+		}
+
+		cout << '\n';
+
+	} while (num != 1);
+
+	return 0;
 }
