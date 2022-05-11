3efedb14a8b94648deb490219036dc175902e916
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-11 12:55:29 +0800

    Committed 2022/05/11

--- Chapter7 - More Data/local_static_variable_2/void_of_static.cpp
+++ Chapter7 - More Data/local_static_variable_2/void_of_static.cpp
@@ -26,8 +26,8 @@ int main() {
 
 int running_average(int i) {
 
-	// Because sum and count are static, they will retain their values between calls to running_average().
-	static int sum = 0, count = 0;
+	// Without using static.
+	int sum = 0, count = 0;
 
 	sum = sum + i;
 
