4a2942df62bcdb78adc6e921f76cf6e049abcaf9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-12 13:46:05 +0800

    Committed 2022/05/12

---
+++ Chapter6 - A Closer Look at Functions/How_Cpp_passes_arguments/Call_by_value.cpp
@@ -0,0 +1,23 @@
+// Changing a call-by-value parameter does not affect the argument.
+
+#include <iostream>
+using namespace std;
+
+double reciprocal(double x);
+
+int main() {
+	double t = 10.0;
+	cout << "Reciprocal of 10.0 is " << reciprocal(t) << "\n";
+
+	cout << "Value of t is still: " << t;
+
+	return 0;
+}
+
+// Return the reciprocal of a value.
+double reciprocal(double x) {
+	
+	x = 1 / x;        // This does not change the value of t inside main().
+
+	return x;
+}
