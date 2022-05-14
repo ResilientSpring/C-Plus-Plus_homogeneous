661a643dcdf9fa615410a2656775ed7711ca5fe1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-14 14:05:18 +0800

    Committed 2022/05/14

---
+++ Chapter4 - Arrays, Strings, and Pointers/strcpy/Substitute.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	char string1[30], string2[30];
+
+	string1 = {"underprivileged"};
+
+}
