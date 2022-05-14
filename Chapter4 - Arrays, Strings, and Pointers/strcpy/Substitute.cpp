6fd55d0517141a60f383f81043fb1e50d0b68fb4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-14 14:50:41 +0800

    Committed 2022/05/14

--- Chapter4 - Arrays, Strings, and Pointers/strcpy/Substitute.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/strcpy/Substitute.cpp
@@ -7,9 +7,19 @@
 #include <iomanip>
 using namespace std;
 
+#define _CRT_SECURE_NO_WARNINGS
+
 int main() {
 	char string1[30], string2[30];
 
-	string1 = {"underprivileged"};
+	strcpy(string1, "underprivilege");
+
+	cout << "string1: " << string1 << "\n";
+
+	strcpy(string2, "C is power programming.");
+
+	cout << "string2: " << string2 << "\n";
+
+	return 0;
 
 }
