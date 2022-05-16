5265f66477d0e919f6922f4739fc4fecca4b4491
Ceiling_roof <ted10014@hotmail.com>
2022-05-16 10:20:51 +0800

    Committed 2022/05/16

---
+++ Chapter8_Classes_and_objects/inline function/Pre-emptive.cpp
@@ -0,0 +1,17 @@
+// Demonstrate inline.
+
+#include <iostream>
+using namespace std;
+
+class c1 {
+	int i;      // private by default.
+
+public:
+	int get_i();
+	void put_i(int j);
+};
+
+
+int main() {
+
+}
