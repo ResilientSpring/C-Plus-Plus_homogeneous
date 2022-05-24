783ad90c53b4d4f64b094af5b72d2452aeec75ae
Ceiling_roof <ted10014@hotmail.com>
2022-05-24 17:29:45 +0800

    Committed 2022/05/24

--- Chapter8_Classes_and_objects/Vehicle_with_inline_functions/fledge.cpp
+++ Chapter8_Classes_and_objects/Vehicle_with_inline_functions/fledge.cpp
@@ -1,4 +1,24 @@
 // Define constructor, destructor, and range() function in-line.
 #include <iostream>
 #include <stdio.h>
+using namespace std;
 
+// Declare the Vehicle class.
+class Vehicle {
+	// These are now private.
+	int passengers; 
+	int fuelcap;
+	int mpg;
+
+public:
+	// This is a constructor for Vehicle.
+	Vehicle(int p, int f, int m) {
+		passengers = p;
+		fuelcap = f;
+		mpg = m;
+	}
+};
+
+int main() {
+
+}
