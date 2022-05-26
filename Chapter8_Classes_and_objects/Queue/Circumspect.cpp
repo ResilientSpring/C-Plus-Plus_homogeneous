3cff6a07eb9a32435135429927651668d6896b2a
Ceiling_roof <ted10014@hotmail.com>
2022-05-26 11:25:07 +0800

    Committed 2022/05/26

--- Chapter8_Classes_and_objects/Queue/Circumspect.cpp
+++ Chapter8_Classes_and_objects/Queue/Circumspect.cpp
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 using namespace std;
 
+const int maxQsize = 100;  // Define the size of the largest queue that can be created.
+
+class Queue {
+	char q[maxQsize];    // This array holds the queue.
+	int size;            // maximum number of elements the queue can hold.
+	int putloc, getloc;  // The put and get indices.
+};
+
 int main() {
 
 }
