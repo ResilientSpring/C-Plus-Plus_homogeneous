cae1208b5dc45d74cb1810e0591cb2cadb7b6a41
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-28 13:58:01 +0800

    Committed 2022/05/28

--- Chapter4 - Arrays, Strings, and Pointers/Array_initialization/Level_up.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Array_initialization/Level_up.cpp
@@ -4,5 +4,20 @@
 using namespace std;
 
 int main() {
+	int i, j;
+	int sqrs[10][2] = {
+		{1, 1},
+		{2, 4},
+		{3, 9},
+		{4, 16},
+		{5, 25},
+		{6, 36},
+		{7, 49},
+		{8, 64},
+		{9, 81},
+		{10, 100}
+	};
 
+	printf("Enter a number between 1 and 10: ");
+	scanf()
 }
