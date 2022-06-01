e1693b59516056d8ebfe35c9c7e5a338deefac54
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-01 10:08:30 +0800

    Committed 2022/06/01

---
+++ Chapter4 - Arrays, Strings, and Pointers/Array of strings/Bipartite.cpp
@@ -0,0 +1,14 @@
+// A simple computerized telephone directory.
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int i;
+	char str[80];
+	char numbers[10][80] = { "Tom", "555-3322", "Mary", "555-8976", "Jon", "555-1037", "Rachel", "555-1400", 
+		"Sherry", "555-8873"};
+
+}
