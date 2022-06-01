7f7334debcfb371cf44068569ca1956143d642e5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-01 12:24:58 +0800

    Committed 2022/06/01

--- Chapter4 - Arrays, Strings, and Pointers/Pointer operators/Base_type.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Pointer operators/Base_type.cpp
@@ -6,10 +6,10 @@ int main() {
 	// In a declaration statement, preceding a variable name with a * causes that variable to be a pointer.
 
 	int total;
-	int* ptr;   // A pointer is an object that can contain a memory address of an integer. 
+	int* ptr;   // This pointer is an object that can contain a memory address of an integer. 
 	int val;
 
-	// ----- End of declaration statements.    See also: Array declaration.
+	// ----- End of declaration statements.    Operation starts
 
 	total = 3200; // Assign 3200 to total.
 	ptr = &total; // get address of total.  & is an operator standing for the memory address of its operand.
