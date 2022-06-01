538da7391763fcbf63c7796baa398900b4ffc691
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-01 15:25:59 +0800

    Committed 2022/06/01

--- Chapter4 - Arrays, Strings, and Pointers/Pointer_base_type_cast/Unmissable.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Pointer_base_type_cast/Unmissable.cpp
@@ -13,4 +13,8 @@ int main() {
 	x = 123.23;
 	p = (int*)&x;   // Use cast to assign double * to int *.
 
+	// These statements won't yield the desired results. 
+	y = *p;  // What will this do? 
+	cout << y;  // What will this print?
+
 }
