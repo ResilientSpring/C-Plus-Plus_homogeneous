e740ff6625345757a3e649e1708e74b2d8fa78dd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-02 19:14:17 +0800

    Committed 2022/06/02

--- Chapter4 - Arrays, Strings, and Pointers/Pointer Value Assignment/Technocratic.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Pointer Value Assignment/Technocratic.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 int main() {
-	// In a declaration statement, preceding a variable name with a * causes that variable to be a pointer.
+	// In a declaration statement, preceding a variable name with a * causes that variable (p) to be a pointer.
 	
 	int *p, num;
 
-	// ----- End of declaration statements. 
+	// ----- End of declaration statements.
+
+	p = &num;   // & is an operator standing for the memory address of its operand.
+
+	*p = 100;   // * is an operator representing the value at the memory address of its operand.
 
 
 }
