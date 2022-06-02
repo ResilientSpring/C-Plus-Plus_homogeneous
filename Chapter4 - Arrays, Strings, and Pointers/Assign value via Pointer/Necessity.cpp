a395c156a32f4d4ebebbc68ad757b7bc09db3348
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-02 17:38:29 +0800

    Committed 2022/06/02

--- Chapter4 - Arrays, Strings, and Pointers/Assign value via Pointer/Necessity.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Assign value via Pointer/Necessity.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int main() {
 	int* p, num;
+
 	p = &num;
-
-
+	*p = 100;                 // Assign num the value 100 through p.
+	cout << num << ' ';
+	(*p)++;                   // Increment num through p.
 }
