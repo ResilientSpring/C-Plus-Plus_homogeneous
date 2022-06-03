65e4e9ccbe391426e2073b94af5b6df0107753a9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-03 12:09:23 +0800

    Committed 2022/06/03

---
+++ Chapter4 - Arrays, Strings, and Pointers/Pointer Arithmetic/Proposer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int *i, j[10];
+
+	double *f, g[10];
+
+
+}
