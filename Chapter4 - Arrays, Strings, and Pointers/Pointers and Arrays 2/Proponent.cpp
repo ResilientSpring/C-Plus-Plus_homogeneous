8706e54b87220360f47f9b665a6cb0b92ff3cae4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-03 13:22:40 +0800

    Committed 2022/06/03

---
+++ Chapter4 - Arrays, Strings, and Pointers/Pointers and Arrays 2/Proponent.cpp
@@ -0,0 +1,11 @@
+// Reverse case using pointer arithmetic.
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int main() {
+
+}
