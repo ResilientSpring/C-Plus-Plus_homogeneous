847760c2101c34970cc1d80559bc03acef4f5fa4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-05 00:01:13 +0800

    Committed 2022/06/05

---
+++ Chapter4 - Arrays, Strings, and Pointers/strlen()/Technocrat.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cctype>
+#include <string.h>
+using namespace std;
+
+int main() {
+	char str[] = "cliche";
+	printf("The length of the string str is %d .\n", strlen(str));
+}
