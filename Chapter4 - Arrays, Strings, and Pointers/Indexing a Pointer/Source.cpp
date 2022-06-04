d2ce7930c796b69f00f7c142098c157c257859cd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-04 12:16:39 +0800

    Committed 2022/06/04

---
+++ Chapter4 - Arrays, Strings, and Pointers/Indexing a Pointer/Source.cpp
@@ -0,0 +1,16 @@
+// Index a pointer as if it were an array.
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <cctype>
+using namespace std;
+
+int main() {
+	char *p;
+	int i;
+	char str[80] = "This Is A Test";
+	printf("Original string: %s \n", str);
+
+
+}
