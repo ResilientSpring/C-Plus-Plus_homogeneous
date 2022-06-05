2177b7f997d06d6833c6706375698377c6519fe6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-05 14:41:22 +0800

    Committed 2022/06/05

---
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 2/fully-fledged.cpp
@@ -0,0 +1,29 @@
+/*
+* Use a 2-D array of pointers to create a dictionary.
+*/
+
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cctype>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	char* dictionary[][2] = { 
+		"pencil", "A writing instrument",
+		"keyboard", "An input device.",
+		"rifle", "A shoulder-fired firearm.",
+		"airplane", "A fixed-wing aircraft",
+		"network", "An interconnected group of computers.",
+		"", ""
+	};
+
+	char word[80];
+	int i;
+
+	printf("Enter word: ");
+
+	scanf("%s", word);
+}
