74de51481bc8a12302772c7863bef522768b3a1a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-07 18:03:39 +0800

    Committed 2022/06/07

---
+++ Chapter8_Classes_and_objects/Pointer to Object/Impediment.cpp
@@ -0,0 +1,26 @@
+// A simple example using an object pointer.
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+#include <cctype>
+#include <string.h>
+
+class P_example {
+	int num;
+public:
+	void set_num(int value) {
+		num = value;
+	}
+
+	void show_num() {
+		printf("%d", num);
+	}
+};
+
+int main() {
+	P_example object, * pointer;
+
+	object.set_num(1);
+	object.show_num();
+}
