d03e830f5652d53a3f76a6d8b4ab83af084d2ae2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-07 18:35:20 +0800

    Committed 2022/06/07

---
+++ Chapter8_Classes_and_objects/Array of objects/Stand down.cpp
@@ -0,0 +1,23 @@
+// Create an array of objects.
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cctype>
+
+class MyClass {
+	int x;
+public:
+	void set_x(int i) {
+		x = i;
+	}
+
+	int get_x() {
+		return x;
+	}
+};
+
+int main() {
+	MyClass ob[4];   // Create an array of objects.
+}
