3e230b8bcacdf59d1309f627af30540c0b15119a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-08 13:09:16 +0800

    Committed 2022/06/08

--- Chapter8_Classes_and_objects/Initializing Object Arrays3/Enact.cpp
+++ Chapter8_Classes_and_objects/Initializing Object Arrays3/Enact.cpp
@@ -12,8 +12,18 @@ public:
 		x = i;
 		y = j;
 	}
+
+	int get_x() {
+		return x;
+	}
+
+	int get_y() {
+		return y;
+	}
 };
 
 int main() {
+
+	MyClass objects[4][2] = { {MyClass(1, 2), MyClass(3, 4)}, {MyClass(5, 6), MyClass object(7, 8)}, };
 
 }
