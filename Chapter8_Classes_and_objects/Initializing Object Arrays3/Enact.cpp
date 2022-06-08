0e352b66386c8abfca39f26a5546b3f623601cd8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-08 12:50:11 +0800

    Committed 2022/06/08

---
+++ Chapter8_Classes_and_objects/Initializing Object Arrays3/Enact.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+
+class MyClass {
+	int x, y;
+
+public:
+	MyClass(int i, int j) {
+		x = i;
+		y = j;
+	}
+};
+
+int main() {
+
+}
