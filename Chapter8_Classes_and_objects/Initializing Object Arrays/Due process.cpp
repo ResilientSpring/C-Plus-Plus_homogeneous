1f33c4a4495d2d78a61a4c1830ce2dbb22c6f542
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-08 10:33:18 +0800

    Committed 2022/06/08

---
+++ Chapter8_Classes_and_objects/Initializing Object Arrays/Due process.cpp
@@ -0,0 +1,16 @@
+// Initialize an array of objects 
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+
+class MyClass {
+	int x;
+public:
+
+};
+
+int main() {
+
+}
