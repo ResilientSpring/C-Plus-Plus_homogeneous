1899b21dbcbcb9bfadc871492a68c93ea2e2de34
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-10 01:26:06 +0800

    Committed 2022/06/10

---
+++ Chapter4 - Arrays, Strings, and Pointers/Multiple Indirection5/Panacea.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cctype>
+#include <string.h>
+#include <iomanip>
+
+int main() {
+
+	// In a declaration statement, preceding a variable name with a * causes that variable (p) to be a pointer.
+
+	int x, * p, ** q;  // A pointer is an object/variable/pointer (of course) that can contain a memory address.
+
+	// ----- End of declaration statements.    Operation starts
+
+	x = 10;
+
+	p = &x;
+
+	q = &p;  // Assign p's memory address to q. (p, of course, has its own memory address.)
+
+	printf("%p", *q);   // %p because the value that sits on p's memory address is x's memory address.
+}
