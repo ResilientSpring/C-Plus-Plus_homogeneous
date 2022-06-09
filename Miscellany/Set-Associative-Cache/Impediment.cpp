d60112887a40f377fb0525317a94bf94206c98b6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-09 11:50:33 +0800

    Committed 2022/06/09

--- Miscellany/Set-Associative-Cache/Impediment.cpp
+++ Miscellany/Set-Associative-Cache/Impediment.cpp
@@ -6,7 +6,18 @@
 #include <iomanip>
 
 class block {
+	bool valid = true;
+	int tag = -1;
+	int count = 0;
+	int address_in_decimal = -1;
+	int address = -1;
+};
 
+class sets {
+public:
+	sets(int SD_SetDegree) {
+
+	}
 };
 
 int main() {
