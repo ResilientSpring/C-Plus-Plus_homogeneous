73856cfd73ee09ff942e75c0b40d388d352b2eb9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-11 13:06:23 +0800

    Committed 2022/06/11

--- Chapter4 - Arrays, Strings, and Pointers/Multiple Indirection7/Implode.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Multiple Indirection7/Implode.cpp
@@ -5,5 +5,12 @@
 #include <string>
 
 int main() {
+	// In a declaration statement, preceding a variable name with a * causes that variable (p) to be a pointer.
 
+	int x, * p, ** q;  // A pointer is an object/variable/pointer (of course) that can contain a memory address.
+
+	// ----- End of declaration statements.    Operation starts
+
+	// As soon as variables are declared, they have their addresses on memory to let the system remember them.
+	printf("%p \n%p  \n%p", &x, &p, &q);
 }
