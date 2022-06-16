69b8f906dbdda2a616c6bf7fbf23b2f7f6180d20
Ceiling_roof <ted10014@hotmail.com>
2022-06-16 23:29:30 +0800

    Committed 2022/06/16

--- Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 9/Defer to someone.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 9/Defer to someone.cpp
@@ -4,5 +4,21 @@
 #include <stdlib.h>
 
 int main() {
-	char 
+	char dictionary[][2] = { {"Pencil", "A writing instrument"} };
+	char dictionary2[] = { "Pencil is a writing instrument." };
+	char dictionary3[][] = { "Pencil is a writing instrument." };
+	char dictionary4[][3] = { {"Pencil", "A writing instrument."} };
+	char dictionary5[][3] = { "Pencil", "A writing instrument." };
+	char dictionary6[10][100] = { {"Pencil", "A writing instrument"}, {"What", "is"} };
+
+    char DirectionPosition[][5][3] = { {"00", "10", "", "01", ""},
+                                    {"01", "11", "", "02", "00"},
+                                    {"02", "12", "", "03", "01"},
+                                    {"03", "13", "", "04", "02"},
+                                    {"04", "14", "", "", "03"},
+                                    {"10", "20", "00", "11", ""},
+                                    {"11", "21", "01", "12", "10"},
+                                    {"12", "22", "02", "13", "11"},
+                                      {"44", "", "34", "", "43"}, };
+
 }
