c6637ffdbfbeb982c146e409ac4824f86fa9ae71
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 17:19:51 +0800

    Committed 2022/06/17

---
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers10/Grievance.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char str[] = "This is a string";
+	char numbers[10][80] = { {"Tom", "555-3322"}};
+	char dictionary[][100] = {"Pencil", "A writing instrument"};
+}
