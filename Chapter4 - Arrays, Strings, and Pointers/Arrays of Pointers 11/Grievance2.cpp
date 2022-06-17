34ce855412fa550074f02a31a6a4f82c597ab95b
Ceiling_roof <ted10014@hotmail.com>
2022-06-17 17:30:29 +0800

    Committed 2022/06/17

--- Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 11/Grievance2.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 11/Grievance2.cpp
@@ -3,5 +3,8 @@
 #include <stdio.h>
 
 int main() {
-
+	char str[] = "This is a string";
+	char str2[] = { "This is a string" };
+	char str3[][100] = { {"This is a string."} };
+	char str4[][100] = { {"This is a string"}, {"Nursing a grievance against sb"}, {"grudge"}};
 }
