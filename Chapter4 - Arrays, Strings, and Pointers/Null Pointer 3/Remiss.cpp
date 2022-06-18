038fe2ba4743cac713007c44ad0d6360d860b364
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-18 16:20:13 +0800

    Committed 2022/06/18

---
+++ Chapter4 - Arrays, Strings, and Pointers/Null Pointer 3/Remiss.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <string.h>
+
+int main() {
+	// In a declaration statement, preceding a variable name with a * causes that variable (p) to be a pointer.
+	float *p;     // A pointer is a variable/object that can hold a memory address (of the specified type).
+	// ----- End of declaration statements.    Operation starts
+
+	p = NULL;
+
+
+}
