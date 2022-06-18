2614eb9dd60233f8c3e64d43985f1df073635251
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-19 00:31:20 +0800

    Committed 2022/06/19

--- Chapter8_Classes_and_objects/Pointer to object ++ --/Deterrent.cpp
+++ Chapter8_Classes_and_objects/Pointer to object ++ --/Deterrent.cpp
@@ -21,4 +21,9 @@ public:
 
 int main() {
 	P_example objectives[2], *p;
+
+	objectives[0].set_num(10);   // Access objects directly.
+	objectives[1].set_num(20);   
+
+	p = &objectives[0];   // obtain pointer to the first element.
 }
