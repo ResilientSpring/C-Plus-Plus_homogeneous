6889169781fbb4854c01002998d272aa2099b3b9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-18 13:52:08 +0800

    Committed 2022/06/18

--- Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 12/Deflect.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 12/Deflect.cpp
@@ -1,5 +1,5 @@
 // Use a 2-D array of pointers to create a dictionary.
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,4 +12,23 @@ int main() {
 		{"airplane", "A fixed-wing aircraft."},
 		{"network", "An interconnected group of computers"}
 	};
+
+//	printf("%s  \n", dictionary[0][0][0]);
+	printf("%c  \n", dictionary[0][0][0]);
+	printf("%s  \n", dictionary[0][0]);
+
+	char word[80];
+
+	printf("Enter word:");
+
+//	scanf("%s  \n", word);
+	scanf("%c  \n", word);
+
+//	printf("You entered %s  \n", *word);
+	printf("You entered %c  \n", *word);
+
+	for (int i = 0; dictionary[i][0][0]; i++)
+	{
+
+	}
 }
