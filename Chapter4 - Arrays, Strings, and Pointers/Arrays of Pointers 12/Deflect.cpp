43ce41fc316dcea055c1b3dc3880f490aa088562
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-18 13:30:31 +0800

    Committed 2022/06/18

--- Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 12/Deflect.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Arrays of Pointers 12/Deflect.cpp
@@ -5,5 +5,11 @@
 #include <stdlib.h>
 
 int main() {
-
+	char dictionary[][2][100] = {
+		{"Pencil", "A writing instrument"},
+		{"keyboard", "An input device"},
+		{"rifle", "A shoulder-fired firearm."}, 
+		{"airplane", "A fixed-wing aircraft."},
+		{"network", "An interconnected group of computers"}
+	};
 }
