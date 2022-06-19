b169237b9ddd78e90b3b208f2fc5901be0fb8a33
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-19 16:00:04 +0800

    Committed 2022/06/19

--- Chapter8_Classes_and_objects/Pointer to objects ++-- 3/Morale.cpp
+++ Chapter8_Classes_and_objects/Pointer to objects ++-- 3/Morale.cpp
@@ -24,5 +24,14 @@ int main() {
 	P_example array_of_P_example[2];
 	
 	array_of_P_example[0] = P_example();
+
+	array_of_P_example[0].set_num(10);
+	array_of_P_example[0].show_num();
+
+	P_example *p;
+
+	p = &array_of_P_example[0];
+
+
 	
 }
