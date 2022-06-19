84340f6ee47f81a5dcbb96b4dacd36caf2bb8891
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-19 14:43:22 +0800

    Committed 2022/06/19

--- Chapter8_Classes_and_objects/Pointers to Objects - 2/Geometry.cpp
+++ Chapter8_Classes_and_objects/Pointers to Objects - 2/Geometry.cpp
@@ -1,1 +1,23 @@
-// 
+// Incrementing and decrementing an object pointer.
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iomanip>
+
+class P_example {
+	int num;
+
+public:
+	void set_num(int val) {
+		num = val;
+	}
+
+	void show_num() {
+		printf("%d \n", num);
+	}
+};
+
+int main() {
+	
+}
