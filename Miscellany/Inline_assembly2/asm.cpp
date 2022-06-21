cddad7c4a0289d0018281aa342df250c7799c740
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-21 16:04:23 +0800

    Committed 2022/06/21

--- Miscellany/Inline_assembly2/asm.cpp
+++ Miscellany/Inline_assembly2/asm.cpp
@@ -8,4 +8,23 @@ int main() {
 	int num1, num2;
 	num1 = 5;
 	num2 = num1;
+
+	printf("num2 = %d  \n", num2);
+
+	_asm {
+		.stack 100h
+		.data
+		num1 sdword ? ;  first number
+		num2 sdword ? ;  second number
+
+		.code
+		main proc
+		mov num1, 5;  initialize num1 with 5
+		mov eax, num1;  load eax with contents of num1
+		mov num2, eax;  store eax in num2
+		ret
+		main endp
+		end
+	}
+
 }
