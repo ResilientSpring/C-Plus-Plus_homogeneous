000ba5e539abbd923b3673be4f76156f697e0934
Ceiling_roof <ted10014@hotmail.com>
2023-01-26 16:10:55 +0800

    Committed 2023/01/26

---
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/StatiMemVar3/Thwart.cpp
@@ -0,0 +1,34 @@
+// Use a static instance variable.
+
+#include <iostream>
+
+using namespace std;
+
+class ShareVar {
+	static int num;  // Declare a static data member. It will be shared by all instances of ShareVar.
+
+public:
+	void setnum(int i) {
+		num = i;
+	}
+
+	void shownum() {
+		cout << num << " ";
+	}
+};
+
+// int ShareVar::num;
+
+int main() {
+	ShareVar a, b;
+
+	a.shownum();
+	b.shownum();
+
+	a.setnum(10);    // Set static num to 10.
+
+	a.shownum();
+	b.shownum();
+
+}
+
