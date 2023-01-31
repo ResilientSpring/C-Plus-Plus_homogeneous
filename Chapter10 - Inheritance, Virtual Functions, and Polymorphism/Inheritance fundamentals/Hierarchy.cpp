0629e189e4f0064b6300a8c9dc18f8d0e8633d83
Ceiling_roof <ted10014@hotmail.com>
2023-02-01 00:02:28 +0800

    Committed 2023/01/31

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance fundamentals/Hierarchy.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance fundamentals/Hierarchy.cpp
@@ -12,3 +12,19 @@ public:
 	}
 
 };
+
+// Triangle is derived from TwoDShape. (Triangle inherits TwoDShape)
+class Triangle : public twoDShape {
+public:
+	char style[20];
+
+	double arcs() {
+
+		// Triangle can refer to members of TwoDShape as if they were part of Triangle.
+		return width * height / 2;    
+	}
+
+	void showStyle() {
+		std::cout << "Triangle is " << style << "\n";
+	}
+};
