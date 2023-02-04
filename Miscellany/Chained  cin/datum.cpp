0426fac33eaa6c1722cf49d53d61486a49ffb0ee
Ceiling_roof <ted10014@hotmail.com>
2023-02-04 15:25:57 +0800

    Committed 2023/02/04

---
+++ Miscellany/Chained  cin/datum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int i;
+	
+	cout << "Please enter an integer value: ";
+
+	cin >> i;
+
+	cout << "The value you entered is " << i << ". \n";
+
+
+}
