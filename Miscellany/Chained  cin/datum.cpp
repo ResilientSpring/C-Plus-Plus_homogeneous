be6558f19011a0e671e92014394707627892e49f
Ceiling_roof <ted10014@hotmail.com>
2023-02-04 15:45:06 +0800

    Committed 2023/02/04

--- Miscellany/Chained  cin/datum.cpp
+++ Miscellany/Chained  cin/datum.cpp
@@ -45,3 +45,12 @@ int main() {
 	cout << "The pair of values you entered are " << c << " and " << d << ".\n";
 
 }
+
+/*
+ References:
+
+ 1. https://github.com/romulus0914/fpga_technology_mapping/blob/master/greedy_mapping.cpp
+ 
+ 2. https://cplusplus.com/doc/tutorial/basic_io/
+
+ */
