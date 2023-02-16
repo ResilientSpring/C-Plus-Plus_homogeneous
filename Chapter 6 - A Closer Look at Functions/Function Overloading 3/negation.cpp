484225a526490a75431ac0eb031c75ff9d3dbf7c
Ceiling_roof <ted10014@hotmail.com>
2023-02-16 23:59:10 +0800

    Committed 2023/02/16

---
+++ Chapter 6 - A Closer Look at Functions/Function Overloading 3/negation.cpp
@@ -0,0 +1,18 @@
+// Create various versions of the neg() function.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+}
+
+// neg() for int
+int neg(int n) {
+	return -n;
+}
+
+// neg() for double.
+double neg(double n) {
+
+}
