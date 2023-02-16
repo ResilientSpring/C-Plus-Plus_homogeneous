ee77723401200be799a4404a5beac2ed8d075507
Ceiling_roof <ted10014@hotmail.com>
2023-02-17 00:04:52 +0800

    Committed 2023/02/17

--- Chapter 6 - A Closer Look at Functions/Function Overloading 3/negation.cpp
+++ Chapter 6 - A Closer Look at Functions/Function Overloading 3/negation.cpp
@@ -3,16 +3,26 @@
 #include <iostream>
 using namespace std;
 
+int neg(int n);
+double neg(double n);
+long neg(long n);
+
+
 int main() {
 	
 }
 
-// neg() for int
+// neg() for int.
 int neg(int n) {
 	return -n;
 }
 
 // neg() for double.
 double neg(double n) {
+	return -n;
+}
 
+// neg() for long.
+long neg(long n) {
+	return -n;
 }
