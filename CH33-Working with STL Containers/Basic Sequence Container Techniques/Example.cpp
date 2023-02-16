5b09fd181d003e51ee42f29654eb7ee3ee3d753a
Ceiling_roof <ted10014@hotmail.com>
2023-02-17 02:22:16 +0800

    Committed 2023/02/17

---
+++ CH33-Working with STL Containers/Basic Sequence Container Techniques/Example.cpp
@@ -0,0 +1,8 @@
+// Demonstrate the basic sequence container operations.
+//
+// This example uses vector, but the same techniques can be applied to any sequence container.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
