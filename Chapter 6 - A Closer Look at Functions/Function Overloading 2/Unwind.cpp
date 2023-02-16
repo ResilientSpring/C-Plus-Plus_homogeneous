9cf6a8c945370c4138f6e093c6010fa940e1064d
Ceiling_roof <ted10014@hotmail.com>
2023-02-16 23:16:53 +0800

    Committed 2023/02/16

---
+++ Chapter 6 - A Closer Look at Functions/Function Overloading 2/Unwind.cpp
@@ -0,0 +1,35 @@
+// Overload a function three times. 
+
+#include <iostream>
+using namespace std;
+
+void f(int i);                // One integer parameter
+void f(int i, int j);         // Two integer parameters
+void f(double k);             // One double parameter
+
+
+int main() {
+
+	f(10);                // call f(int)
+
+	f(10, 20);            // call f(int, int)
+
+	f(12.23);             // call f(double)
+
+}
+
+// To overload a function, the type or number of parameters of each overloaded function must differ. 
+
+void f(int i) {
+	cout << "In f(int), i is " << i << "\n";
+}
+
+void f(int i, int j) {
+	cout << "In f(int, int), j is " << j;
+	cout << ", i is " << j << "\n";
+}
+
+
+void f(double k) {
+	cout << "In f(double), k is " << k << '\n';
+}
