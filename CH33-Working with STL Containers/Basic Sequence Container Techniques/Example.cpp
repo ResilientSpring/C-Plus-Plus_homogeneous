44fc8e09dd835b3e2d49c189a0505a8f205aa50e
Ceiling_roof <ted10014@hotmail.com>
2023-02-17 17:59:00 +0800

    Committed 2023/02/17

--- CH33-Working with STL Containers/Basic Sequence Container Techniques/Example.cpp
+++ CH33-Working with STL Containers/Basic Sequence Container Techniques/Example.cpp
@@ -9,5 +9,14 @@ using namespace std;
 int main() {
 
 	// Declare an empty vector that can hold char objects.
+	vector<char> v;
+
+	// Declare an iterator to a vector<char>.
+	vector<char>::iterator itr;
+
+	// Obtain an iterator to the start of v.
+	itr = v.begin();
+
+	// Insert characters into v. An iterator to the inserted object is returned.
 
 }
