4cdd960726b12bad55b185ac42f789d705cfed6e
Ceiling_roof <ted10014@hotmail.com>
2023-02-18 12:13:30 +0800

    Committed 2023/02/18

--- Chapter 6 - A Closer Look at Functions/Function Overloading 4/min().cpp
+++ Chapter 6 - A Closer Look at Functions/Function Overloading 4/min().cpp
@@ -4,5 +4,37 @@
 using namespace std;
 
 int main() {
+	int i = 10, j = 32;
+
 
 }
+
+// min() for ints. Return the smallest value.
+int min(int a, int b) {
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+// min() for chars -- ignore case.
+char min(char a, char b) {
+	if (tolower(a) < tolower(b))
+		return a;
+	else
+		return b;
+}
+
+/*
+	min() for int pointers.
+	Compare values and return the pointer to the smallest value.
+*/
+int* min(int* a, int* b) {
+	if (*a < *b)
+		// return *a;
+		return a;
+	else
+		// return *b;
+		return b;
+}
+
