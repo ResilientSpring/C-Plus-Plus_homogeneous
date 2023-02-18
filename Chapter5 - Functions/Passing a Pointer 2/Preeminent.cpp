3d38439371d9b5fc7df0b6b2df06128b5f82f586
Ceiling_roof <ted10014@hotmail.com>
2023-02-18 13:46:38 +0800

    Committed 2023/02/18

---
+++ Chapter5 - Functions/Passing a Pointer 2/Preeminent.cpp
@@ -0,0 +1,22 @@
+// Pass a pointer to a function.
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+// f() takes an int * pointer as a parameter.
+void f(int* j);    // f() declares a pointer parameter.
+
+int main() {
+	int i;
+
+	f(&i);      // pass a pointer.
+
+	cout << i;
+
+}
+
+// f() receives a pointer to an int
+void f(int* j) {
+	*j = 100;       // variable pointed to by j is assigned 100.
+}
