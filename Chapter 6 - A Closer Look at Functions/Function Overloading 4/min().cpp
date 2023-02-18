786b2cd4c141b7fc743d55b11462e6baf02c18c9
Ceiling_roof <ted10014@hotmail.com>
2023-02-18 12:17:14 +0800

    Committed 2023/02/18

--- Chapter 6 - A Closer Look at Functions/Function Overloading 4/min().cpp
+++ Chapter 6 - A Closer Look at Functions/Function Overloading 4/min().cpp
@@ -3,10 +3,17 @@
 #include <iostream>
 using namespace std;
 
+int min(int a, int b);
+char min(char a, char b);
+int* min(int* a, int* b);
+
+
 int main() {
 	int i = 10, j = 32;
 
-
+	cout << "min('X', 'a'): " << min('X', 'a') << endl;
+	cout << "min(9, 3): " << min(9, 3) << endl;
+	cout << "*min(&i, &j): " << *min(&i, &j) << endl;
 }
 
 // min() for ints. Return the smallest value.
