2591b7f0c1b484e7749062bbe91097b0263caa57
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-20 20:32:05 +0800

    Committed 2023/02/20

---
+++ Chapter5 - Functions/Passing an array 3/Intimidate.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void display(int num[10]);
+void displays(int num[]);  // Recall that C++ performs no bounds checking on arrays. 
+                           // Thus, display(int num[10]) is the same as display(int num[]).
+void display(int* num);
+
+
+int main() {
+	int t[10];
+
+	for (int i = 0; i < 10; ++i)
+		t[i] = i;
+
+	display(t);  // Pass array t to a function.
+}
+
+// Print some numbers.
+void display(int num[10]) {
+
+	for (int i = 0; i < 10; i++)
+		cout << num[i] << ' ';
+}
+
+void displays(int num[]) {      // Parameter declared as an unsized array.
+
+	for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
+		cout << num[i] << ' ';
+}
+
+void display(int* num) {       // Parameter declared as a pointer.
+
+	for (int i = 0; i < 10; i++)
+		cout << num[i] << " ";
+}
