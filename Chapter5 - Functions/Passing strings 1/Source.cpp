fd3e29f0f2e9710d89fc34d2fe8120a95750f496
Ceiling_roof <ted10014@hotmail.com>
2023-02-22 20:21:01 +0800

    Committed 2023/02/22

--- Chapter5 - Functions/Passing strings 1/Source.cpp
+++ Chapter5 - Functions/Passing strings 1/Source.cpp
@@ -10,9 +10,11 @@ int main() {
 
 	}
 
-	for (int i = 0; i < 10; i++) {
-		cout << "{" << endl;
+
+	cout << "{" << endl;
+
+	for (int i = 0; i < 10; i++) 		
 		cout << nums[i] << " ";
-		cout << endl << "}";
-	}
+
+	cout << endl << "}";
 }
