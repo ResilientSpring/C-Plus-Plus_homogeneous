4ae71a1f5e9ab4083beb67a696da0f4327d4634b
Ceiling_roof <ted10014@hotmail.com>
2023-02-24 01:33:01 +0800

    Committed 2023/02/24

---
+++ CH2-DataTypesAndOperators/Characters/Heed.cpp
@@ -0,0 +1,11 @@
+// This program displays the alphabet.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char letter;
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		cout << letter;
+}
