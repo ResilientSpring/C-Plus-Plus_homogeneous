a7667638c27969b973121056d48e7f192c2f890c
Ceiling_roof <ted10014@hotmail.com>
2023-02-23 16:21:29 +0800

    Committed 2023/02/23

--- Chapter4 - Arrays, Strings, and Pointers/Pointer arithmetic 1-2/Scenic.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Pointer arithmetic 1-2/Scenic.cpp
@@ -7,7 +7,7 @@ int main() {
 
 	double g[10];
 
-	// Add values 0 through 9 to the memory address of the first element of the array. 
+	// Add integer 0 to 9 to the memory address of the first element of the array. 
 	// (Print memory address of each element of the array.)
 	for (int x = 0; x < 10; x++)
 		cout << j + x << " " << g + x << endl;
