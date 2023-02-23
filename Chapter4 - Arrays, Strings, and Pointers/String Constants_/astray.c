0d99618a160074376bfa518892e391eec6345a9c
Ceiling_roof <ted10014@hotmail.com>
2023-02-24 02:57:13 +0800

    Committed 2023/02/24

---
+++ Chapter4 - Arrays, Strings, and Pointers/String Constants_/astray.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+
+int main() {
+	char* ptr;
+
+	ptr = "Pointers add power to C++. \n";  // ptr is assigned the address of this string constant.
+
+	printf(ptr);
+}
