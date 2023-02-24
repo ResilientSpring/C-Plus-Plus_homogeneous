71bf95ec2f039ec86cad1887c62a3fc8abf0b742
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-25 01:31:31 +0800

    Committed 2023/02/25

--- Chapter5 - Functions/Passing strings 6/Xanadu3.cpp
+++ Chapter5 - Functions/Passing strings 6/Xanadu3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	char str[10];
 
-	for (char letter = 'A' && int i = 0; letter < 'J' && i < 10; letter++ && i++) {
+	for (char letter = 'A', int i = 0; letter < 'J', i < 10; letter++, i++) {
 
 		*(str + i) = letter;
 	}
