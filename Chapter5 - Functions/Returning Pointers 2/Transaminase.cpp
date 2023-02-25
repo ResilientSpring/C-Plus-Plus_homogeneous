d4fde46d3fa180e84f010c8c7c6bb9543b32942d
Ceiling_roof <ted10014@hotmail.com>
2023-02-26 01:32:10 +0800

    Committed 2023/02/26

---
+++ Chapter5 - Functions/Returning Pointers 2/Transaminase.cpp
@@ -0,0 +1,19 @@
+// Return a Pointer.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
+
+
+// Succeeding a function's return type with asterisk * causes the function to return 
+// a pointer of that return type.
+char* get_substring(char *sub, char *str) {  // get_substring() returns a char pointer.
+	int t;
+	char *p, *p2, *start;
+
+
+}
+
