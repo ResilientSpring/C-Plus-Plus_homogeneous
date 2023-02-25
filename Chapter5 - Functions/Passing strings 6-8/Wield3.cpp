8f35a4ee5c77cc520d9f5ce2fcbf38369b849392
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-25 21:50:58 +0800

    Committed 2023/02/25

---
+++ Chapter5 - Functions/Passing strings 6-8/Wield3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[10];
+
+	char letter;
+	int i;
+
+	for (letter = 65, i = 0; letter < 74, i < 10; letter++, i++) {
+
+		*(str + i) = letter;
+	}
+
+
+
+
+
+
+	cout << str << endl;
+}
+
+/*
+To declare two variables of different types in a for loop:
+
+https://stackoverflow.com/a/2687408/
+*/
