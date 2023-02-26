fb2f4863fa25df35458970d701f2bcf25d4a22cc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-26 16:44:25 +0800

    Committed 2023/02/26

---
+++ Chapter5 - Functions/Returning Pointers 5/Assort.cpp
@@ -0,0 +1,49 @@
+// Return a Pointer.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char* get_substring(char* sub, char* str);
+
+int main() {
+	char* substring;
+
+	char search[] = "one two three four";
+	char search_for[] = "three";
+
+	// Null-terminated string (char) differs from string constant (const char).
+	substring = get_substring(search_for, search);
+
+	cout << "substring found: " << substring;
+}
+
+
+// Succeeding a function's return type with asterisk * causes the function to return 
+// a pointer of that return type.
+char* get_substring(char* sub, char* str) {  // get_substring() returns a char pointer.
+	int t;
+	char* p, * p2, * start;
+
+	for (t = 0; str[t]; t++) {
+
+		p = &str[t];  // Reset pointers
+
+		start = p;
+
+		p2 = sub;
+
+		while (*p2 && *p2 == *p) {  // Check for substring
+			p++;
+			p2++;
+		}
+
+		// if at end of p2 (i.e., substring), then a match has been found.
+		if (!*p2)
+			return start;    // return pointer to the beginning of substring.
+	}
+
+	return 0; // no match found.
+}
+
