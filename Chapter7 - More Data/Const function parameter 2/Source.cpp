69ef7c2f1baadc409ca44e638a7807321756259e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-26 23:38:44 +0800

    Committed 2023/02/26

--- Chapter7 - More Data/Const function parameter 2/Source.cpp
+++ Chapter7 - More Data/Const function parameter 2/Source.cpp
@@ -6,6 +6,20 @@ void cube(int* n, int num);
 
 int main() {
 
+	int i, nums[10];
+
+	for (i = 0; i < 10; i++)
+		nums[i] = i + 1;
+
+	cout << "Original contents: "; 
+	
+	for (i = 0; i < 10; i++)
+		cout << nums[i] << " ";
+	
+	cout << "\n";
+
+
+
 }
 
 void cube(int* n, int num) {
