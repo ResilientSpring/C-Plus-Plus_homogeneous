b78055822a99b1524b68ed885b709d35161483cf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-28 18:11:13 +0800

    Committed 2023/02/28

--- Chapter4 - Arrays, Strings, and Pointers/Array of constant char/Searchable.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Array of constant char/Searchable.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-	const char* dictionary[][2] = { "Pencil" };
+	const char* dictionary[][52] = { "Pencil", "A writing instrument"};
+
+	char word[80];
+
+	cout << "Enter word: ";
+
+	cin >> word;
+
+	for (int i = 0; *dictionary[i][0]; i++) {
+
+	}
+
 }
