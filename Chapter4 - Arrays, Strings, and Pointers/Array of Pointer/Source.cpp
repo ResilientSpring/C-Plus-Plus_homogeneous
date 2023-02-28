ecfcae3105520823b2308a64504c4546a03e3aa4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-02-28 16:38:46 +0800

    Committed 2023/02/28

--- Chapter4 - Arrays, Strings, and Pointers/Array of Pointer/Source.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Array of Pointer/Source.cpp
@@ -1,5 +1,12 @@
+#include <iostream>
 using namespace std;
 
 int main() {
+	int integers[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 };
 
+	// Declare an array of pointers of type integer.
+	int* integer_pointers[] = { &integers[0], &integers[1], &integers[2] };
+
+
+	cout << integer_pointers;
 }
