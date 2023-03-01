0e163169d89895cc9b9b1656ad671cc05dcbaacb
Ceiling_roof <ted10014@hotmail.com>
2023-03-01 17:34:46 +0800

    Committed 2023/03/01

---
+++ Chapter5 - Functions/Passing strings 6-1_/Xanadu3_.c
@@ -0,0 +1,20 @@
+
+
+int main() {
+	char str[10];
+
+	char letter;
+	int i;
+
+	for (letter = 'A', i = 0; letter < 'J', i < 10; letter++, i++) {
+
+		*(str + i) = letter;
+	}
+
+
+
+
+
+
+	prinf(str);
+}
