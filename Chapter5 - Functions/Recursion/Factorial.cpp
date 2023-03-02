a332cb0117dc1eeb0b937057918097bd58b819a7
Ceiling_roof <ted10014@hotmail.com>
2023-03-02 12:13:46 +0800

    Committed 2023/03/02

--- Chapter5 - Functions/Recursion/Factorial.cpp
+++ Chapter5 - Functions/Recursion/Factorial.cpp
@@ -8,7 +8,16 @@ int main() {
 }
 
 
-// Recursive version
-int factr(int n) {
+// Iterative version
+int fact(int n) {
 
+	int answer;
+
+	answer = 1;
+
+	for (int t = 1; t <= n; t++)
+
+		answer = answer * t;
+
+	return answer;
 }
