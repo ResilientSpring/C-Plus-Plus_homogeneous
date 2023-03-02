7b61044094a15034a6dc3662ffa4473fe9159ac0
Ceiling_roof <ted10014@hotmail.com>
2023-03-02 19:48:59 +0800

    Committed 2023/03/02

---
+++ Chapter5 - Functions/Recursion 8/reverse()_2.cpp
@@ -0,0 +1,25 @@
+// Print a string backwards using recursion.
+
+#include <iostream>
+
+void reverse(char* s);
+
+int main() {
+
+	char str[] = "this is a test";
+
+	reverse(str);
+
+}
+
+// Print string backwards.
+void reverse(char* s) {
+
+	if (*s)
+		reverse(s + 1);
+	else
+		return;
+
+
+	cout << *s;
+}
