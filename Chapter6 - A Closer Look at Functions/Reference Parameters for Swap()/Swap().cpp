fc3e9046094d444f9aa6c08fb2942d2400b488e1
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 21:27:56 +0800

    Committed 2023/03/04

---
+++ Chapter6 - A Closer Look at Functions/Reference Parameters for Swap()/Swap().cpp
@@ -0,0 +1,13 @@
+// Use reference parameters to create the swap() function.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
+
+
+/* Here, swap() is defined as using call-by-reference, not call-by-value. Thus, it can exchange
+   the two arguments it is called with.
+*/
