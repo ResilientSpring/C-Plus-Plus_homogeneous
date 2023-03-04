2ce11c76551ce33cf0eac9fc1c72f3f74dd19389
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 21:31:11 +0800

    Committed 2023/03/04

--- Chapter6 - A Closer Look at Functions/Reference Parameters for Swap()/Swap().cpp
+++ Chapter6 - A Closer Look at Functions/Reference Parameters for Swap()/Swap().cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& x, int& y);
+
 int main() {
 
 }
@@ -11,3 +13,12 @@ int main() {
 /* Here, swap() is defined as using call-by-reference, not call-by-value. Thus, it can exchange
    the two arguments it is called with.
 */
+void swap(int &x, int &y) {
+	
+	int temp;
+
+	// Use references to exchange the values of the arguments.
+	temp = x;
+	x = y;
+	y = temp;
+}
