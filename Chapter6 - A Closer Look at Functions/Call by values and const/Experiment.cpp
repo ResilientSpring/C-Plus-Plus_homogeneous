c98910eb11593d2e3886acadaac981573ab59be4
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 18:24:33 +0800

    Committed 2023/03/04

--- Chapter6 - A Closer Look at Functions/Call by values and const/Experiment.cpp
+++ Chapter6 - A Closer Look at Functions/Call by values and const/Experiment.cpp
@@ -1,4 +1,4 @@
-// Changing a call-by-value parameter does not affect the argument.
+ // Experiment to see whether integer and double are a constant by default.
 
 #include <iostream>
 using namespace std;
@@ -6,7 +6,9 @@ using namespace std;
 double reciprocal(double *x);
 
 int main() {
+
 	double t = 10.0;
+
 	cout << "Reciprocal of 10.0 is " << reciprocal(&t) << "\n";
 
 	cout << "Value of t is still: " << t << "\n";
@@ -14,10 +16,10 @@ int main() {
 	return 0;
 }
 
-// Return the reciprocal of a value.
+
 double reciprocal(double *x) {
 
-	*x = 1 / *x;        // This does not change the value of t inside main().
+	*x = 1 / *x;        
 
 	return *x;
 }
