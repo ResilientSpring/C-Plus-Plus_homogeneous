010d91f826698c3fea8fff82d97a519fecab82ec
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 23:04:04 +0800

    Committed 2023/03/04

---
+++ Chapter6 - A Closer Look at Functions/Recall 5-9：Returning Pointers/Heed.cpp
@@ -0,0 +1,16 @@
+// Return a pointer.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
+
+// Return pointer to substring or null if not found.
+// Succeeding a function's return type with asterisk * causes the function to return 
+// a pointer of that return type.
+char* get_substring() {
+
+}
+
