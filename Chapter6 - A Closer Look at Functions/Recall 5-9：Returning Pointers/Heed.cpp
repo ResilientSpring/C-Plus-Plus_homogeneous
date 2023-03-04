9ed60ec78008b1bb528360ecf2486f17e7ae73a8
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 23:11:27 +0800

    Committed 2023/03/04

--- Chapter6 - A Closer Look at Functions/Recall 5-9：Returning Pointers/Heed.cpp
+++ Chapter6 - A Closer Look at Functions/Recall 5-9：Returning Pointers/Heed.cpp
@@ -9,7 +9,11 @@ int main() {
 
 // In declaration, succeeding a function's return type with asterisk * causes the function to  
 // return a pointer of that return type.
-char* get_substring() {              // Return pointer to substring or null if not found.
+char* get_substring(char *substring, char *string) {
+
+	// Return pointer to substring or null if not found.
+
+
 
 }
 
