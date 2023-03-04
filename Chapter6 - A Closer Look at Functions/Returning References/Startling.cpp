472293428c1e0070aa7f8d58232ec46d34d55e48
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 22:32:47 +0800

    Committed 2023/03/04

---
+++ Chapter6 - A Closer Look at Functions/Returning References/Startling.cpp
@@ -0,0 +1,16 @@
+// Returning a reference.
+
+#include <iostream>
+using namespace std;
+
+double& f();
+
+int main() {
+
+}
+
+// This function returns a reference to a double.
+double& f() {
+	return val;
+}
+
