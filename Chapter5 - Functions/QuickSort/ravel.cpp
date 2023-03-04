9f0155c3ebd926ece9c5ac7c2fa5c12e6c8a8910
Ceiling_roof <ted10014@hotmail.com>
2023-03-04 15:52:12 +0800

    Committed 2023/03/04

---
+++ Chapter5 - Functions/QuickSort/ravel.cpp
@@ -0,0 +1,12 @@
+
+void quicksort(char* items, int len);
+
+int main() {
+	
+}
+
+
+// Se up a call to the actual sorting function.
+void quicksort(char *items, int len) {
+	qs(items, 0, len - 1);
+}
