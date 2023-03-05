dc55a127febab8200b341ca7a875718af35a3a79
Ceiling_roof <ted10014@hotmail.com>
2023-03-05 11:37:24 +0800

    Committed 2023/03/05

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generic Func/template.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generic Func/template.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 using namespace std;
 
-
-template<class x> void swapargs(X &a, X &b) {
+// This is a function template.
+// A generic function that exchanges the values of its arguments. 
+template<class x> void swapargs(X &a, X &b) {  // Here, X is the generic data type.
 
 }
 
