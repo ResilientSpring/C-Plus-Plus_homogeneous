d726effa03ad0718a5a0770c6ee71cc345171431
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-05 19:39:25 +0800

    Committed 2023/03/05

--- CH33-Working with STL Containers/Basic Sequence Container Techniques 3/Example3.cpp
+++ CH33-Working with STL Containers/Basic Sequence Container Techniques 3/Example3.cpp
@@ -13,7 +13,7 @@ int main() {
 	// Declare an empty vector that can hold char objects.
 	vector<char> v;
 
-	// iterator is a class inherited from vector. Declare an object of iterator class.
+	// Declare an object of iterator class.
 	vector<char>::iterator itr;
 
 	// Obtain an iterator to the start of v.
