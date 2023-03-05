dd6355d0b7153086747e9cfe17617f5168d592f2
Ceiling_roof <ted10014@hotmail.com>
2023-03-05 11:45:17 +0800

    Committed 2023/03/05

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generic Func/template.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generic Func/template.cpp
@@ -16,5 +16,19 @@ template<class x> void swapargs(X &a, X &b) {  // Here, X is the generic data ty
 
 
 int main() {
+	int i = 10, j = 20;
+	float x = 10.1, y = 23.3;
+	char a = 'x', b = 'z';
 
+	cout << "Original i, j: " << i << " " << j << endl;
+	cout << "Original x, y: " << x << " " << y << endl;
+	cout << "Original a, b: " << a << " " << b << endl;
+
+	swapargs(i, j); // swap integers
+	swapargs(x, y); // swap floats
+	swapargs(a, b); // swap chars
+
+	cout << "Swapped i, j: " << i << " " << j << endl;
+	cout << "Swapped x, y: " << x << " " << y << endl;
+	cout << "Swapped a, b: " << a << " " << b << endl;
 }
