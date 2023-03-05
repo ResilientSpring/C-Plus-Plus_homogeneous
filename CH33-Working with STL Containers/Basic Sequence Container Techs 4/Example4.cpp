203c69c9b24405bd29ef17aafbcd301e464e021b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-05 18:37:02 +0800

    Committed 2023/03/05

---
+++ CH33-Working with STL Containers/Basic Sequence Container Techs 4/Example4.cpp
@@ -0,0 +1,49 @@
+// Demonstrate the basic sequence container operations.
+//
+// This example uses vector, but the same techniques can be applied to any sequence container.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void show(const char* msg, vector<char> vect);
+
+int main() {
+
+	// Declare an empty vector that can hold char objects.
+	vector<char> v;
+
+	// iterator is a class inherited from vector. Declare an object of iterator class.
+	vector<char>::iterator itr;
+
+	// Obtain an iterator to the start of v.
+	itr = v.begin();
+
+	// Insert character 'A' into v at the point specified by the iterator and then return the iterator at 'A'.
+	itr = v.insert(itr, 'A');
+
+	// Insert character 'B' immediately before the element specified by the iterator, and then return the iterator at 'B'.
+	itr = v.insert(itr, 'B');
+
+	// Insert character 'C' immediately before the element specified by the iterator, and then return the iterator at 'C'.
+	v.insert(itr, 'C');
+
+	// Display the contents of v.
+	show("The contents of v: ", v);
+
+	// Declare a reverse iterator.
+	vector<char>::reverse_iterator ritr;
+}
+
+
+// Display the contents of a vector<char> by using an iterator.
+void show(const char* msg, vector<char> vect) {
+
+	vector<char>::iterator itr;
+
+	cout << msg;
+
+	for (itr = vect.begin(); itr != vect.end(); ++itr)
+		cout << *itr << " ";
+	cout << "\n";
+}
