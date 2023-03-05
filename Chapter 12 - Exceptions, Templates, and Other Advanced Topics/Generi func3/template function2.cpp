0046a477a483f3803767c4361924646449f2e6fa
Ceiling_roof <ted10014@hotmail.com>
2023-03-06 02:17:22 +0800

    Committed 2023/03/06

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generi func3/template function2.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generi func3/template function2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-template<class Int> void swapargs(Int a, Int b) { 
+template<class I> void swapargs(I a, I b) { 
 
-	Int intermediary;
+	I intermediary;
 	intermediary = a;
 	a = b;
 	b = intermediary;
@@ -13,18 +13,10 @@ template<class Int> void swapargs(Int a, Int b) {
 
 int main() {
 	int i = 10, j = 20;
-	float x = 10.1, y = 23.3;
-	char a = 'x', b = 'z';
 
 	cout << "Original i, j: " << i << " " << j << endl;
-	cout << "Original x, y: " << x << " " << y << endl;
-	cout << "Original a, b: " << a << " " << b << endl;
 
 	swapargs(i, j); // swap integers
-	swapargs(x, y); // swap floats
-	swapargs(a, b); // swap chars
 
 	cout << "Swapped i, j: " << i << " " << j << endl;
-	cout << "Swapped x, y: " << x << " " << y << endl;
-	cout << "Swapped a, b: " << a << " " << b << endl;
 }
