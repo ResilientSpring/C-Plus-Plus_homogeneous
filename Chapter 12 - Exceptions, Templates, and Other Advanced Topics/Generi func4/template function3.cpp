7fac58da679283716f0a0d5b955e2e336902efd8
Ceiling_roof <ted10014@hotmail.com>
2023-03-06 13:31:49 +0800

    Committed 2023/03/06

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generi func4/template function3.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Generi func4/template function3.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void swapargs(int a = 1, int b = 2);
+
 int main() {
+
+	int i = 10, j = 20;
+
+	cout << "Original value i, j: " << i << " " << j << endl;
+
+	swapargs(i, j);
+
+	cout << "Swapped i, j: " << i << " " << j << endl;
 
 }
 
