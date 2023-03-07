3a6a305b3994e4a12f007828fffb1172462a7f26
Ceiling_roof <ted10014@hotmail.com>
2023-03-07 20:49:02 +0800

    Committed 2023/03/07

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/ConstrNinheri/Constructors and inheritance.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/ConstrNinheri/Constructors and inheritance.cpp
@@ -15,6 +15,23 @@ public:
 		cout << "Width and height are " << width << " and " << height << "\n";
 	}
 
+	// accessor functions
+	double getWidth() {
+		return width;
+	}
+	
+	double getHeight() {
+		return height;
+	}
+
+	void setWidth(double w) {
+		width = w;
+	}
+
+	void setHeight(double h) {
+		height = h;
+	}
+
 };
 
 int main() {
