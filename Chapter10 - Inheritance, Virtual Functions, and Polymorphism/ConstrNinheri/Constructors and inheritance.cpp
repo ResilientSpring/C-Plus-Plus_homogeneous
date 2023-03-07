99e6048c3bc239d70e5c0dc24f81a64359479209
Ceiling_roof <ted10014@hotmail.com>
2023-03-07 20:45:40 +0800

    Committed 2023/03/07

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/ConstrNinheri/Constructors and inheritance.cpp
@@ -0,0 +1,22 @@
+// Add a constructor to triangle.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// A class for two-dimensional objects.
+class TwoDShape {
+
+	// these are private
+	double width;
+	double height;
+
+public:
+	void showDim() {
+		cout << "Width and height are " << width << " and " << height << "\n";
+	}
+
+};
+
+int main() {
+
+}
