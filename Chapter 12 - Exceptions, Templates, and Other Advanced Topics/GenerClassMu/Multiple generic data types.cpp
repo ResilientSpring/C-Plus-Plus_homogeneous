774f03e9f4732b1fc9d0d79e42c07c6c28862539
Ceiling_roof <ted10014@hotmail.com>
2023-03-09 23:43:47 +0800

    Committed 2023/03/09

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/GenerClassMu/Multiple generic data types.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/GenerClassMu/Multiple generic data types.cpp
@@ -3,6 +3,26 @@
 #include <iostream>
 using namespace std;
 
+template <class T1, class T2>
+
+class MyClass {
+
+	T1 i;
+	T2 j;
+
+public:
+
+	MyClass(T1 i, T2 j) {
+		this->i = i;
+		this->j = j;
+	}
+
+	void show() {
+		cout << i << ' ' << j << '\n';
+	}
+
+};
+
 int main() {
 
 }
