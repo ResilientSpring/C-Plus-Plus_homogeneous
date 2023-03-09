b4046126a658241f597925e7b3cc66640deee8a3
Ceiling_roof <ted10014@hotmail.com>
2023-03-09 15:56:30 +0800

    Committed 2023/03/09

--- Miscellany/Bin_/Mapping.cpp
+++ Miscellany/Bin_/Mapping.cpp
@@ -2,18 +2,20 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char** argv) { // [1]
+int K;
+
+int main(int argc, char **argv) { // [1]
 
 	if (argc != 4) {
 
-		cout << "Usage: ./mapping <path_to_the_input_blif> <LUT_size_(K)> <output_file_name> \n";
-
+		cout << "Usage:  \n";
+		cout << "./mapping <path_to_the_input_blif> <LUT_size_(K)> <output_file_name>" << endl;
 		exit(1);
 	}
 
 	string blif_file = argv[1];
 
-
+	K = stoi(argv[2]);
 }
 
 /*
