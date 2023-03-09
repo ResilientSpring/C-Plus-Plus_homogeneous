37103ca9c4efc1ef1262d0c15271c246350e4819
Ceiling_roof <ted10014@hotmail.com>
2023-03-09 11:43:20 +0800

    Committed 2023/03/09

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/UnInitialized/Member variables.cpp
@@ -0,0 +1,69 @@
+// Add a constructor to triangle.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// A class for two-dimensional objects.
+class TwoDShape {
+
+public:
+
+	double width;
+	double height;
+
+	void showDim() {
+		cout << "Width and height are " << width << " and " << height << "\n";
+	}
+
+	// accessor functions
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	void setWidth(double w) {
+		width = w;
+	}
+
+	void setHeight(double h) {
+		height = h;
+	}
+
+};
+
+// Triangle is derived from TwoDShape.
+class Triangle : public TwoDShape {
+
+	char style[20];  // now private.
+
+public:
+
+	// Constructor for Triangle.
+	Triangle(const char* str, double w, double h) {
+
+		// Initialize the base class portion.
+		setWidth(w);
+		setHeight(h);
+
+		// Initialize the derived class portion.
+		strcpy(style, str);
+
+	}
+
+
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+
+};
+
+int main() {
+
+	TwoDShape twoDshape;
+
+	cout << twoDshape.height;
+	
+}
