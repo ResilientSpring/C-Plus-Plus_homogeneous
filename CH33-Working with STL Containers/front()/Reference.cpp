4abd1bd046b1127e093e6200d366bc7e3d526a81
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-12 00:45:41 +0800

    Committed 2023/03/12

--- CH33-Working with STL Containers/front()/Reference.cpp
+++ CH33-Working with STL Containers/front()/Reference.cpp
@@ -7,4 +7,12 @@ int main() {
 
 	vector<int> vector_of_integer;
 
+	vector<int>::iterator itr;
+
+	itr = vector_of_integer.begin();
+
+	itr = vector_of_integer.insert(itr, 2);
+
+	vector_of_integer.push_back(5);
+
 }
