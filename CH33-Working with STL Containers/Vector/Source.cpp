ef8744c6c3c79ecb1bb61ac6718d0ffb06b4ca59
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-11 18:33:51 +0800

    Committed 2023/03/11

--- CH33-Working with STL Containers/Vector/Source.cpp
+++ CH33-Working with STL Containers/Vector/Source.cpp
@@ -9,4 +9,7 @@ int main() {
 	// Declare a vector that has an initial capacity of 10.
 	vector<int> v(10);
 
+	// Assign its elements some values. 
+	for (unsigned i = 0; i < v.size(); ++i)
+		v[i] = i * i;
 }
