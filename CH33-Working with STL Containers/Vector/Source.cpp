1bd176717c510827b5b82822b37f85b27337fe6e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-11 18:21:06 +0800

    Committed 2023/03/11

---
+++ CH33-Working with STL Containers/Vector/Source.cpp
@@ -0,0 +1,12 @@
+// Demonstrate vector.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	// Declare a vector that has an initial capacity of 10.
+	vector<int> v(10);
+
+}
