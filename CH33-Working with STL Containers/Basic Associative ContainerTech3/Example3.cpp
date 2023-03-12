54009d770f59df846834ffa7141f766f8d0c5e66
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-12 17:48:49 +0800

    Committed 2023/03/12

---
+++ CH33-Working with STL Containers/Basic Associative ContainerTech3/Example3.cpp
@@ -0,0 +1,47 @@
+// Demonstrate the basic associative container operations.
+
+// This example uses map, but the same basic techniques can be applied to any associative container.
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+void show(const char *message, map<string, int> m);
+
+
+int main() {
+
+	// Declare an empty map that holds key/value pairs in which the key is a string and the value is an int.
+	map<string, int> m;
+
+	// Insert characters into m. An iterator to the inserted object is returned. 
+	m.insert(pair<string, int>("Alpha", 100));
+	m.insert(pair<string, int>("Gamma", 300));
+	m.insert(pair<string, int>("Beta", 200));
+
+	// Declare an iterator to a map<string, itr>.
+	map<string, int>::iterator itr;
+
+	// Display the first element in m.
+
+	itr = m.begin();
+
+	cout << "Here is the first key/value pair in m: " << itr->first << ", " << itr->second << endl;
+
+	// Display the last element in m.
+	itr = m.end();
+	--itr;
+	cout << "Here is the last key/value pair in m: " << itr->first << ", " << itr->second << endl << endl;
+
+}
+
+// Display the contents of a map<string, int> by using an iterator.
+void show(const char *message, map<string, int> m) {
+	
+	cout << message;
+
+
+
+}
