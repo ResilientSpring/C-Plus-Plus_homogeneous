a9c3e3c707e9d1a07a691608d190f4f3a57b34ad
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-12 17:19:23 +0800

    Committed 2023/03/13

---
+++ CH33-Working with STL Containers/Basic Associative Container Tech/Example.cpp
@@ -0,0 +1,15 @@
+// Demonstrate the basic associative container operations.
+
+// This example uses map, but the same basic techniques can be applied to any associative container.
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+
+	// Declare an empty map that holds key/value pairs in which the key is a string and the value is an int.
+	map<string, int> m;
+
+}
