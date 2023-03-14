fd023c722883f3138af635a9e33ea23c53577b23
Ceiling_roof <ted10014@hotmail.com>
2023-03-14 21:40:52 +0800

    Committed 2023/03/14

--- CH44-Algorithms/find()/Heed.cpp
+++ CH44-Algorithms/find()/Heed.cpp
@@ -8,4 +8,22 @@ using namespace std;
 
 int main() {
 
+	vector<string> v;
+	vector<string>::iterator itr;
+
+	v.push_back("one");
+	v.push_back("two");
+	v.push_back("three");
+	v.push_back("four");
+	v.push_back("five");
+	v.push_back("six");
+
+	cout << "Contents of v: ";
+
+	for (unsigned i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+
+	cout << endl << endl;
+
+
 }
