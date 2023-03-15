c8be2b61cf23482efc2bb136287d763a9f349185
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-15 18:13:33 +0800

    Committed 2023/03/15

---
+++ CH 10-IO System/Reading and Writing Text Files 2/Critical Skill 11-8-2.cpp
@@ -0,0 +1,24 @@
+// Write to file.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+	ofstream stream("test");
+
+	if (!stream) {
+
+		cout << "Cannot open file. \n";
+
+		return 1;
+	}
+
+	stream << 10 << " " << 123.23 << endl;
+	stream << "This is a short text file.";
+
+	cout << "A stream is disassociated from a file through a close operation.";
+	stream.close();
+
+}
