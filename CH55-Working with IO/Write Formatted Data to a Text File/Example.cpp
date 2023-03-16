0fe1262f81fa70f2339fdc9e95f4ad6cc05ed2c7
Ceiling_roof <ted10014@hotmail.com>
2023-03-16 16:25:15 +0800

    Committed 2023/03/16

--- CH55-Working with IO/Write Formatted Data to a Text File/Example.cpp
+++ CH55-Working with IO/Write Formatted Data to a Text File/Example.cpp
@@ -2,7 +2,19 @@
 
 #include <iostream>
 #include <fstream>
+using namespace std;
 
 int main() {
+
+	// Create an ofstream object and attempt to open the file test.dat
+	ofstream fout("test.dat");
+
+	// Verify that the file has been successfully opened.
+	if (!fout) {
+		
+		cout << "Cannot open.";
+
+		return 1;
+	}
 
 }
