7589da665b851ac0b74c4f40d7ce4c93350c7e6a
Ceiling_roof <ted10014@hotmail.com>
2023-03-16 22:55:48 +0800

    Committed 2023/03/16

--- Chapter4 - Arrays, Strings, and Pointers/Reading a string from keyboard 4/intangible asset.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Reading a string from keyboard 4/intangible asset.cpp
@@ -1,9 +1,12 @@
-// Display a file using gets()
+// Using gets() to read a string from keyboard.
 
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
 int main() {
+
+	char str[80];
+
 
 }
