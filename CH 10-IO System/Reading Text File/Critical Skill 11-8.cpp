28c9eddc9ea482d3fd001b8d3972e9fc57a23a27
Ceiling_roof <ted10014@hotmail.com>
2023-03-16 10:34:58 +0800

    Committed 2023/03/16

--- CH 10-IO System/Reading Text File/Critical Skill 11-8.cpp
+++ CH 10-IO System/Reading Text File/Critical Skill 11-8.cpp
@@ -5,5 +5,28 @@
 using namespace std;
 
 int main() {
+	
+	char ch;
+	int i;
+	float f;
+	char str[80];
+
+	ifstream in("test");
+
+	if (!in) {
+		cout << "Cannot open file. \n";
+		return 1;
+	}
+
+	in >> i;
+	in >> f;
+	in >> ch;
+	in >> str;
+
+	cout << i << " " << f << " " << ch << "\n";
+	cout << str;
+
+	in.close();
+
 
 }
