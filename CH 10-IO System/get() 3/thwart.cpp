225b8813130e377ba25469f173015ece55c90481
Ceiling_roof <ted10014@hotmail.com>
2023-03-17 02:34:25 +0800

    Committed 2023/03/17

--- CH 10-IO System/get() 3/thwart.cpp
+++ CH 10-IO System/get() 3/thwart.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 	// while loop
 	while (input_stream) {  // in will be false when eof is reached.
 
-		input_stream.get(ch);  // get() reads a character from stream by putting that char in ch.
+		input_stream.get(ch); // get() reads a character from stream by putting that char in ch.
 
 		if (input_stream)
 			cout << ch;
