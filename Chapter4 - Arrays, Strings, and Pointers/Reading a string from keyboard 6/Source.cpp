f9b181049eebd82c5f666b2d1d3e5602838fa7b3
Ceiling_roof <ted10014@hotmail.com>
2023-03-17 00:03:45 +0800

    Committed 2023/03/17

--- Chapter4 - Arrays, Strings, and Pointers/Reading a string from keyboard 6/Source.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Reading a string from keyboard 6/Source.cpp
@@ -11,4 +11,27 @@ int main() {
 	cout << "Enter a string: " << endl;
 
 	gets_s(str);
+
+
+	/*  In this case, although I treated 
+			
+			gets_s() 
+			
+			as if it were 
+			
+			gets()
+
+
+	, I have no idea about which requirement did gets_s(str) meet,
+
+			char* gets_s(char *buffer, rsize_t size)
+
+			or
+
+			char* gets_s( char (&_Buffer)[_Size])
+
+			?		
+	
+	*/
+
 }
