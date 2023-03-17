cb58209ef155e64e04e688cd0c80132266e4361d
Ceiling_roof <ted10014@hotmail.com>
2023-03-17 11:11:17 +0800

    Committed 2023/03/17

--- CH55-Working with IO/Read Formatted Data from a Text File/Exemplify.cpp
+++ CH55-Working with IO/Read Formatted Data from a Text File/Exemplify.cpp
@@ -5,4 +5,20 @@ using namespace std;
 
 int main() {
 
+	int i, n;
+
+	double d;
+
+	string str;
+
+	// Create an ifstream object and attempt to associate stream with (open) the file test.dat
+	ifstream input_stream("test.dat");
+	
+	if (!input_stream) {
+
+		cout << "Cannot open the file. " << endl;
+		
+		return 1;
+	}
+
 }
