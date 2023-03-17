903b6bd1a1d2b730517c4cffe7c0c10568ea4209
Ceiling_roof <ted10014@hotmail.com>
2023-03-17 13:14:35 +0800

    Committed 2023/03/17

--- CH 10-IO System/getline() test/entice.cpp
+++ CH 10-IO System/getline() test/entice.cpp
@@ -6,7 +6,11 @@ int main() {
 
 	ofstream output_stream("enticement.txt");
 
-	if (output_stream) {
+	if (!output_stream) {
+
+		cout << "Cannot associate the stream with the designated file.";
+
+		return 1;
 
 	}
 
