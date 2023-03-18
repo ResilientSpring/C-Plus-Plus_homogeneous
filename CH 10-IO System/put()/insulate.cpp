ea6d8c9ebe176e3c930d32f38e6dad13ff535b93
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-18 15:24:33 +0800

    Committed 2023/03/18

--- CH 10-IO System/put()/insulate.cpp
+++ CH 10-IO System/put()/insulate.cpp
@@ -8,6 +8,21 @@ int main() {
 
 	const char *p = "hello there. \n";
 
+	ofstream out("test", ios::binary);
 
+	if (!out) {
+		
+		cout << "Tried to associate stream with your file but didn't succeed.";
+
+		return 1;
+
+	}
+
+	// Write characters until the null-terminator is reached.
+	while (*p)
+	{
+		out.put(*p++);
+
+	}
 
 }
