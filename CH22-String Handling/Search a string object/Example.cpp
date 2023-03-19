3fad0b6b98a57f6dcd1c3cdfa99d54bf13fb04c9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-19 19:32:37 +0800

    Committed 2023/03/19

--- CH22-String Handling/Search a string object/Example.cpp
+++ CH22-String Handling/Search a string object/Example.cpp
@@ -8,5 +8,10 @@ int main() {
 
 	string::size_type index;
 
+	// Create a string.
+	string str("one two three, one two three");
+	string str2;
+
+	cout << "String to be searched: " << str << endl << endl;
 
 }
