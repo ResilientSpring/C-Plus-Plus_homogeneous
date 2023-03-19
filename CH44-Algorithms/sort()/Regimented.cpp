8f14d8d714e7264823b7581196f3e5ff21b4ad36
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-03-20 00:36:30 +0800

    Committed 2023/03/20

---
+++ CH44-Algorithms/sort()/Regimented.cpp
@@ -0,0 +1,12 @@
+// Demonstrate the sort() algorithm.
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+}
