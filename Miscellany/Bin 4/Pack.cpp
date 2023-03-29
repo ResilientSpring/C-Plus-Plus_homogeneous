da7646e1581a2ce50f3bb675eb102ee9ae231e05
Ceiling_roof <ted10014@hotmail.com>
2023-03-29 18:29:14 +0800

    Committed 2023/03/29

---
+++ Miscellany/Bin 4/Pack.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <cstring>
+#include <sstream>
+using namespace std;
+
+int K;
+int number_of_primary_inputs = 0;
+int number_of_primary_outputs = 0;
+int number_of_intermediate_nodes = 0;
+int reading_the_line_from_hereon = 0;
+int vertex_count;
+string blif_name, intermediate;
+
+char model[] = ".model";
+
+map<string, vertex *> find_Node;
+
+vector<string> primary_inputs;
+vector<string> intermediate_nodes;
+vector<string> primary_outputs;
+
+vector<string> operand;
+
+
+class vertex {
+public:
+	string ID;
+
+	bool in_use;
+	vector<int> fanins;
+	vector<vertex> inputs;
+	int fanout;
+	int number_of_fanouts;
+
+	int label;
+	int operate;
+
+	int type;
+	int level;
+	int and_or_inv;
+
+	vertex *first = NULL;
+	vertex *second = NULL;
+
+} sitting_vertex;
+
+
+
+class model {
+
+public:
+
+	string name;
+	vector<vertex> node;
+	vector<vertex> LUT;
+
+} sitting_model;
+
+
+
+vector<vertex> intermediate_inputs;
+
+
+
+int main(int argc, char **argv) { // [1]
+
+	if (argc != 4) {
+
+		cout << "Usage:  \n";
+		cout << "./mapping <path_to_the_input_blif> <LUT_size_(K)> <output_file_name>" << endl;
+		exit(1);
+	}
+
+	string input_blif = argv[1];
+	read_blif(input_blif);
+
+	K = stoi(argv[2]);
+
+	cout << "Input File: " << argv[1] << endl;
+	cout << "K: " << argv[2] << endl;
+	cout << "Output File: " << argv[3] << endl;
+}
+
+
+void read_blif(string blif) {
+
+	string sitting_line;
+
+	ifstream input_stream;
+
+	input_stream.open(blif);
+
+	if (!input_stream) {
+
+		cout << "Cannot open the file" << endl;
+
+		return;
+	}
+
+
+
+
+}
+
+
+
+
+
+/*
+*
+* References:
+*
+* 1. https://stackoverflow.com/a/645111
+* 2. https://github.com/Lyeuiechang/Advanced_Logic_Synthesis/blob/main/Final%20Project/Upload%20Ready/main.cpp
+* 3. https://github.com/yunchenlo/FlowMap_Tech_Mapping/blob/master/main.cpp
+* 4. https://github.com/romulus0914/fpga_technology_mapping/blob/master/greedy_mapping.cpp
+* 5. https://github.com/Koyama-Tsubasa/Advanced_Logic_Synthesis/blob/main/Final%20Project/submit/src/map.cpp
+* 6. https://github.com/abhishekjain1991-zz/Raja_Raman_and_Wong_Clustering/blob/master/Source_code/RandW_clustering.cpp
+* 7. https://github.com/weitunglin/blif_parser/blob/main/blif_parser.cpp
+* 8. https://github.com/Ohpaipai/BLIF_CAD/blob/master/Source.cpp
+* 9. https://github.com/alan861130/Logic-synthesis/blob/main/src/map.cpp#L419
+* 10. https://github.com/rushhuang/NTHU_ALS_Final/blob/main/LEDA/map.cpp
+* 11. https://stackoverflow.com/questions/5590381/easiest-way-to-convert-int-to-string-in-c
+
+*/
