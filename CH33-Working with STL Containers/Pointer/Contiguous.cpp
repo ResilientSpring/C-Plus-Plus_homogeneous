30d7e324387888a98066fb1a1e6524f5d96641ca
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-04-10 23:18:44 +0800

    Committed 2023/04/10

--- CH33-Working with STL Containers/Pointer/Contiguous.cpp
+++ CH33-Working with STL Containers/Pointer/Contiguous.cpp
@@ -12,5 +12,18 @@ int main() {
 	v.push_back(8);
 	v.push_back(10);
 
+	
+}
+
+void show(const char *message, vector<int> v) {
+
+	cout << message << endl;
+
+	vector<int>::iterator itr;
+
+	for (itr = v.begin(); itr != v.end(); itr++)
+		cout << *itr << endl;
+
+	cout << endl;
 
 }
