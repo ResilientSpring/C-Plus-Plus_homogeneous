cd4c6e70b1653111d257f314ac35e3df31eca839
Ceiling_roof <ted10014@hotmail.com>
2023-04-13 20:23:57 +0800

    Committed 2023/04/13

--- Miscellany/Uniqueness/Uniquely.cpp
+++ Miscellany/Uniqueness/Uniquely.cpp
@@ -6,4 +6,20 @@ using namespace std;
 
 int main() {
 
+	ifstream input_stream;
+
+	input_stream.open("alu4.aag");
+
+	if (!input_stream)
+		cout << "File cannot be opened." << endl;
+	else
+		cout << "File opened successfully." << endl;
+
+	string line;
+
+	while (getline())
+	{
+
+	}
+
 }
