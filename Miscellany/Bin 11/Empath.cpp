4f1525fbf20b24400cbe503f7afaecf826241df2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-04-15 16:54:36 +0800

    Committed 2023/04/15

--- Miscellany/Bin 11/Empath.cpp
+++ Miscellany/Bin 11/Empath.cpp
@@ -474,7 +474,7 @@ void mapper1() {
 			LUTs[i] = NULL;
 
 		// Because topologically_sorted_nodes_in_a_tree[i] returns an entire queue, 
-		// it is safer to precede the receiving variable with &; it's okay not doing it, though.
+		// it seems safer to precede the receiving variable with &; it's tested okay not doing it, though.
 		queue<int> Queue = topologically_sorted_nodes_in_a_tree[i];
 		//		queue<int> *Queue = &trees_topologically_sorted[i];
 		vector<int> *tree_inv = trees_inverse[i];  // tree_inv is declared as an array of int vector (trees).
