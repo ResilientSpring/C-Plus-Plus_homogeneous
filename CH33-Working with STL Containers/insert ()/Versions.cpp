d0aa6c0d2e2855a0127c8378348389eb404f7df7
Ceiling_roof <ted10014@hotmail.com>
2023-04-17 21:50:46 +0800

    Committed 2023/04/17

--- CH33-Working with STL Containers/insert ()/Versions.cpp
+++ CH33-Working with STL Containers/insert ()/Versions.cpp
@@ -10,4 +10,26 @@ int main() {
 
 	cout << v.size();
 
+	
+
+	cout << endl << endl;
+
+	vector<int> v2;
+
+
+
+}
+
+void show(const char *message, vector<int> vec) {
+
+	cout << message << endl;
+
+	vector<int>::iterator itr;
+
+	for (itr = vec.begin(); itr != vec.end(); itr++)
+		cout << *itr << " ";
+
+
+	cout << endl;
+
 }
