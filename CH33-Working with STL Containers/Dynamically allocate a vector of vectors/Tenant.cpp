63c0f11fe1cbdd88b68f9c20c69594f1a7acbac2
Ceiling_roof <ted10014@hotmail.com>
2023-04-17 18:40:34 +0800

    Committed 2023/04/17

---
+++ CH33-Working with STL Containers/Dynamically allocate a vector of vectors/Tenant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<vector<int>> v;
+
+	vector<int> v2;
+
+	v2.push_back(4);
+	v2.push_back(6);
+	v2.push_back(8);
+
+}
