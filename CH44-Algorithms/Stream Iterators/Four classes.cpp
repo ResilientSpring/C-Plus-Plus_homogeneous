e0214e9c406a74e9b4d6ba6093caed9238aff6e2
Ceiling_roof <ted10014@hotmail.com>
2023-04-24 06:07:15 +0800

    Committed 2023/04/24

---
+++ CH44-Algorithms/Stream Iterators/Four classes.cpp
@@ -0,0 +1,12 @@
+// Use istream_iterator and ostream_iterator to read from cin and write to cout.
+
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+int main() {
+
+
+
+}
