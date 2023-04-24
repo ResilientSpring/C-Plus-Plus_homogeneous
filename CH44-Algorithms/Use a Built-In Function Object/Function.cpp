9652fad732d424b3ce1d030093cd6452d8db48c1
Ceiling_roof <ted10014@hotmail.com>
2023-04-24 23:55:02 +0800

    Committed 2023/04/24

---
+++ CH44-Algorithms/Use a Built-In Function Object/Function.cpp
@@ -0,0 +1,13 @@
+// Demonstrate negate and multiplies function objects.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+
+	vector<int> v, v2, result(10);
+
+}
