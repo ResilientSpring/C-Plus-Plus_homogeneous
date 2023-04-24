b3a807d203830518f5ffc1b494dd895f753f44ec
Ceiling_roof <ted10014@hotmail.com>
2023-04-25 06:30:36 +0800

    Committed 2023/04/25

---
+++ CH44-Algorithms/transform()/obviate.cpp
@@ -0,0 +1,5 @@
+// Demonstrate the transform() algorithm.
+//
+// Both versions of transform() are used within the program.
+
+// The first alters the sequence of doubles so that it contains reciprocal values.
