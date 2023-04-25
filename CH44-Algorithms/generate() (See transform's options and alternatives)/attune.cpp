dc78be7204e4f9f5a7511d3ca170e25c3c59e91c
Ceiling_roof <ted10014@hotmail.com>
2023-04-25 23:24:36 +0800

    Committed 2023/04/25

---
+++ CH44-Algorithms/generate() (See transform's options and alternatives)/attune.cpp
@@ -0,0 +1,22 @@
+// Generate a sequence.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	vector<double> v(5);
+
+	// Generate a sequence.
+	generate(v.begin, v.end(), )
+
+}
+
+// A simple generator function that generates the powers of 2.
+double power_of_two() {
+	static double value = 1.0;
+	double t;
+
+	t = value;
+}
