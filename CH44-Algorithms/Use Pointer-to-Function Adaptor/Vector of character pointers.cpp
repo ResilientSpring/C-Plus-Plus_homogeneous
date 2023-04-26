5ecef02a24e82d2ae437efe7ca2704e5dc1113b1
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 05:33:26 +0800

    Committed 2023/04/27

--- CH44-Algorithms/Use Pointer-to-Function Adaptor/Vector of character pointers.cpp
+++ CH44-Algorithms/Use Pointer-to-Function Adaptor/Vector of character pointers.cpp
@@ -9,4 +9,10 @@ using namespace std;
 
 int main() {
 
+	// A vector of character pointers that point to character strings (character arrays).
+	vector<const char *> v;
+	vector<char *>::iterator itr;
+
+	v.push_back("One");
+
 }
