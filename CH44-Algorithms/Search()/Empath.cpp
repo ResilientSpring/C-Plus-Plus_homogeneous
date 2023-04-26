ac8ae38fd145db86088b7ac3b8ec6cd13c15d34c
Ceiling_roof <ted10014@hotmail.com>
2023-04-26 22:49:30 +0800

    Committed 2023/04/26

---
+++ CH44-Algorithms/Search()/Empath.cpp
@@ -0,0 +1,19 @@
+// Demonstrate search()
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+	deque<string> log;
+	deque<string> break_in;
+	deque<string>::iterator itr;
+
+
+
+
+
+}
