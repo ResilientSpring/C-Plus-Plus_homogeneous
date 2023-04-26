68fad20754c74560a0fca7e130e039f377e0bf1b
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 01:44:41 +0800

    Committed 2023/04/27

---
+++ CH44-Algorithms/Binder/Synthetically.cpp
@@ -0,0 +1,24 @@
+// Demonstrate bind2nd()
+
+#include <iostream>
+#include <list>
+#include <functional>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	list<int> lst;
+	list<int>::iterator res_itr;
+
+	for (unsigned i = 1; i < 20; i++)
+		lst.push_back(i);
+
+}
+
+
+// Show range of elements.
+template<class InIter>
+void show_range(const char *message, InIter start, InIter end) {
+
+}
