d82c5c52346d6dd48e410da4e7e167cff45c1d8b
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 01:48:15 +0800

    Committed 2023/04/27

--- CH44-Algorithms/Binder/Synthetically.cpp
+++ CH44-Algorithms/Binder/Synthetically.cpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 using namespace std;
 
+template<class InIter>
+void show_range(const char *message, InIter start, InIter end);
+
 int main() {
 
 	list<int> lst;
@@ -13,6 +16,9 @@ int main() {
 
 	for (unsigned i = 1; i < 20; i++)
 		lst.push_back(i);
+
+	show_range("Original sequence:\n", lst.begin(), lst.end());
+	cout << endl;
 
 }
 
