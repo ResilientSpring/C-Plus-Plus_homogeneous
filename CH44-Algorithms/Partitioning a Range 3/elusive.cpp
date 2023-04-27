0d8d208bac213134d18187aca41128c94cd198bf
Ceiling_roof <ted10014@hotmail.com>
2023-04-28 01:16:24 +0800

    Committed 2023/04/28

--- CH44-Algorithms/Partitioning a Range 3/elusive.cpp
+++ CH44-Algorithms/Partitioning a Range 3/elusive.cpp
@@ -5,7 +5,9 @@
 #include <functional>
 using namespace std;
 
-bool comparison(double a, double b);
+bool comparison(double a);
+
+double average;
 
 int main() {
 
@@ -15,7 +17,7 @@ int main() {
 
 	cout << endl;
 
-	auto average = accumulate(temperature.begin(), temperature.end(), 0.0) / temperature.size();
+	average = accumulate(temperature.begin(), temperature.end(), 0.0) / temperature.size();
 
 	cout << endl << "Average temperature: " << average << endl << endl;
 
@@ -26,11 +28,21 @@ int main() {
 	cout << endl;
 }
 
-bool comparison(double a, double b) {
+bool comparison(double a) {
 
-	if (a < b)
+	if (a < average)
 		return true;
 	else
 		return false;
 
 }
+
+
+// Note:
+// 1. How to access variables in main() from other function?
+// As explained in the comments, it is better to change the architecture of your program. 
+// However, if you still want to opt for a simple solution, declare your x as in global scope. [1]
+
+
+// Reference: 
+// 1. https://stackoverflow.com/questions/60786249/how-to-access-variables-in-main-cpp-from-function-in-mainwindow-cpp
