933cbfd85d9440741af1edd0e34e97e3da290b97
Ceiling_roof <ted10014@hotmail.com>
2023-04-28 02:48:59 +0800

    Committed 2023/04/28

---
+++ CH44-Algorithms/Partitioning a Range - 2/gender.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+using gender = char;
+using first = string;
+using second = string;
+using Name = tuple<first, second, gender>;
+
+int main() {
+
+	vector<Name> names{ make_tuple("Dan", "Old", 'm'), make_tuple("Ann", "Old", 'f'),
+		make_tuple("Ed", "Old", 'm'), make_tuple("Jan", "Old", 'f'), make_tuple("Edna", "Old", 'f') };
+
+	partition(names.begin(), names.end(), [](const Name &name) {return get<2>(name) == 'f'; });
+
+}
