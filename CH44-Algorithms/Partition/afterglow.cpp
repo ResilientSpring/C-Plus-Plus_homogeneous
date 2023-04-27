78d535a51f5da8e6c5df5395de0829ed0caa80cb
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 21:19:00 +0800

    Committed 2023/04/27

--- CH44-Algorithms/Partition/afterglow.cpp
+++ CH44-Algorithms/Partition/afterglow.cpp
@@ -1,4 +1,5 @@
 // Using partition() and equal_range() to find duplicates of a value in a range
+
 #include <iostream>                              // For standard streams
 #include <list>                                  // For list container
 #include <algorithm>                             // For copy(), partition()
@@ -8,6 +9,12 @@ int main() {
 
 	std::list<int> values{ 17, 11, 40, 13, 22, 54, 48, 70, 22, 61, 82, 78, 22, 89, 99, 92, 43 };
 
+	// Output the elements in their original order
+	std::cout << "The elements in the original sequence are:\n";
+	std::copy(std::begin(values), std::end(values), std::ostream_iterator<int> {std::cout, " "});
+	std::cout << std::endl;
 
 
 }
+
+// Source: https://github.com/Apress/using-cpp-standard-template-libraries/blob/master/Chapter%206/Ex6_05.cpp
