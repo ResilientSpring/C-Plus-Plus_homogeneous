eccc74ba156a1e0e79bb64a465e030a459feb7b5
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 19:40:52 +0800

    Committed 2023/04/27

---
+++ CH44-Algorithms/Partition/afterglow.cpp
@@ -0,0 +1,13 @@
+// Using partition() and equal_range() to find duplicates of a value in a range
+#include <iostream>                              // For standard streams
+#include <list>                                  // For list container
+#include <algorithm>                             // For copy(), partition()
+#include <iterator>                              // For ostream_iterator
+
+int main() {
+
+	std::list<int> values{ 17, 11, 40, 13, 22, 54, 48, 70, 22, 61, 82, 78, 22, 89, 99, 92, 43 };
+
+
+
+}
