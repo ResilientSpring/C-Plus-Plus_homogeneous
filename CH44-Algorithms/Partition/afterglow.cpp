5d21b4fdbe8af9ac84a87185e0f079c2cf436860
Ceiling_roof <ted10014@hotmail.com>
2023-04-27 21:21:25 +0800

    Committed 2023/04/27

--- CH44-Algorithms/Partition/afterglow.cpp
+++ CH44-Algorithms/Partition/afterglow.cpp
@@ -4,16 +4,25 @@
 #include <list>                                  // For list container
 #include <algorithm>                             // For copy(), partition()
 #include <iterator>                              // For ostream_iterator
+using namespace std;
 
 int main() {
 
-	std::list<int> values{ 17, 11, 40, 13, 22, 54, 48, 70, 22, 61, 82, 78, 22, 89, 99, 92, 43 };
+	list<int> values{ 17, 11, 40, 13, 22, 54, 48, 70, 22, 61, 82, 78, 22, 89, 99, 92, 43 };
 
 	// Output the elements in their original order
-	std::cout << "The elements in the original sequence are:\n";
-	std::copy(std::begin(values), std::end(values), std::ostream_iterator<int> {std::cout, " "});
-	std::cout << std::endl;
+	cout << "The elements in the original sequence are:\n";
+	copy(std::begin(values), std::end(values), std::ostream_iterator<int> {std::cout, " "});
+	cout << std::endl;
 
+
+	int wanted{ 22 };  // What we are looking for
+
+	// Partition the values with (value < wanted)
+	partition(begin(values), end(values), [wanted](double value) { return value < wanted; });
+	
+	// Partition the values with !(wanted < value)
+	partition(begin(values), end(values), [wanted](double value) { return !(wanted < value); });
 
 }
 
