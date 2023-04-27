18c4f5d7d6426daeda13ceb4f0a2605daee39daa
Ceiling_roof <ted10014@hotmail.com>
2023-04-28 01:07:24 +0800

    Committed 2023/04/28

--- CH44-Algorithms/Partitioning a Range 3/elusive.cpp
+++ CH44-Algorithms/Partitioning a Range 3/elusive.cpp
@@ -5,6 +5,8 @@
 #include <functional>
 using namespace std;
 
+bool comparison(double a, double b);
+
 int main() {
 
 	vector<double> temperature{ 65, 75, 56, 48, 31, 28, 32, 29, 40, 41, 44, 50 };
@@ -16,5 +18,19 @@ int main() {
 	auto average = accumulate(temperature.begin(), temperature.end(), 0.0) / temperature.size();
 
 	cout << endl << "Average temperature: " << average << endl << endl;
+
+	partition(temperature.begin(), temperature.end(), comparison);
+
+	copy(begin(temperature), end(temperature), ostream_iterator<double>{cout, " "});
+
+	cout << endl;
+}
+
+bool comparison(double a, double b) {
+
+	if (a < b)
+		return true;
+	else
+		return false;
 
 }
