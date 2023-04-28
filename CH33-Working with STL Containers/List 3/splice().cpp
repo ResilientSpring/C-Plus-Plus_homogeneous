e14dbc7f04712f5feb3150a6adcf9a8f3a65e3d6
Ceiling_roof <ted10014@hotmail.com>
2023-04-28 15:02:52 +0800

    Committed 2023/04/28

---
+++ CH33-Working with STL Containers/List 3/splice().cpp
@@ -0,0 +1,10 @@
+// From book <Using the C++ Standard Template Libraries> Chapter 2 Using Sequence Containers.
+// Page 70.
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+
+}
