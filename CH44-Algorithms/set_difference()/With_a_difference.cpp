afaeba86af366797c64a4dcc5b26cd8b8e1fa391
Ceiling_roof <ted10014@hotmail.com>
2023-05-04 01:38:50 +0800

    Committed 2023/05/04

--- CH44-Algorithms/set_difference()/With_a_difference.cpp
+++ CH44-Algorithms/set_difference()/With_a_difference.cpp
@@ -2,27 +2,28 @@
 #include <iostream>     // std::cout
 #include <algorithm>    // std::set_difference, std::sort
 #include <vector>       // std::vector
+using namespace std;
 
 int main() {
     int first[] = { 5,10,15,20,25 };
     int second[] = { 50,40,30,20,10 };
-    std::vector<int> v(10);                      // 0  0  0  0  0  0  0  0  0  0
-    std::vector<int>::iterator it;
+    vector<int> v(10);                      // 0  0  0  0  0  0  0  0  0  0
+    vector<int>::iterator it;
 
-    std::sort(first, first + 5);     //  5 10 15 20 25
-    std::sort(second, second + 5);   // 10 20 30 40 50
+    sort(first, first + 5);     //  5 10 15 20 25
+    sort(second, second + 5);   // 10 20 30 40 50
 
-    it = std::set_difference(first, first + 5, second, second + 5, v.begin());
+    it = set_difference(first, first + 5, second, second + 5, v.begin());
     //  5 15 25  0  0  0  0  0  0  0
     v.resize(it - v.begin());                      //  5 15 25
 
-    std::cout << "The difference has " << (v.size()) << " elements:\n";
+    cout << "The difference has " << (v.size()) << " elements:\n";
     for (it = v.begin(); it != v.end(); ++it)
-        std::cout << ' ' << *it;
-    std::cout << '\n';
+        cout << ' ' << *it;
+    cout << '\n';
 
     return 0;
 }
 
 
-// Reference: https://cplusplus.com/reference/algorithm/set_difference/
+// Adapted from: https://cplusplus.com/reference/algorithm/set_difference/
