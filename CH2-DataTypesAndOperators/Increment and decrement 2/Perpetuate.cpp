60a4e44744426fc74ff9e23d2a2105308805f74c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-01 16:57:15 +0800

    Committed 2023/08/01

--- CH2-DataTypesAndOperators/Increment and decrement 2/Perpetuate.cpp
+++ CH2-DataTypesAndOperators/Increment and decrement 2/Perpetuate.cpp
@@ -10,12 +10,12 @@ int main() {
 	y = ++x;
 
 	cout << "y is " << y << "\n";
+	cout << "x is " << x << "\n";
+
+	x = 10;
 
 	y = x++;
 
-	cout << "y is " << y << "\n";
 
 
-
-	cout << "x is " << x << "\n";
 }
