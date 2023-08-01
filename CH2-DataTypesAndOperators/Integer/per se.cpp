9bb0d84985fb2cd793cc69e79d694485b027db18
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-01 15:04:47 +0800

    Committed 2023/08/01

--- CH2-DataTypesAndOperators/Integer/per se.cpp
+++ CH2-DataTypesAndOperators/Integer/per se.cpp
@@ -9,4 +9,12 @@ int main() {
 
 	short unsigned int j;  // an unsigned short integer.
 
+	// 60000 is within the range of an unsigned short int, but is typically outside the range of a signed short int.
+	j = 60000;  
+
+	// Thus, it will be interpreted as a negative value when assigned to i.
+	i = j; 
+
+	cout << i << " " << j;
+
 }
