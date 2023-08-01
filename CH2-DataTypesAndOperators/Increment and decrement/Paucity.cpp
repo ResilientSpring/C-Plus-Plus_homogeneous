0d74bd73bb29e8a468ca2f2ada80a794ad6ea39d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-01 16:40:58 +0800

    Committed 2023/08/01

---
+++ CH2-DataTypesAndOperators/Increment and decrement/Paucity.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x, y;
+
+	x = 10;
+
+	y = ++x;
+
+	cout << "y is " << y << "\n";
+
+	y = x++;
+
+	cout << "y is " << y << "\n";
+
+}
