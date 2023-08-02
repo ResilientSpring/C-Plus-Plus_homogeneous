26fba0dcd4f5161a0add2e93ad8ad0e3aad78bfb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-02 13:34:04 +0800

    Committed 2023/08/02

---
+++ CH2-DataTypesAndOperators/Project 2-3 Compute the Regular Payments on a Loan/Pricing model.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double Principal;    // original principal
+	double Interest_rate; 
+	double PayPerYear;
+	double NumYears;
+	double Payment;    // the regular payment
+	double number, denon;  // temporary work variables. 
+	double base, exponent;    // base and exponent for call to pow()
+
+}
