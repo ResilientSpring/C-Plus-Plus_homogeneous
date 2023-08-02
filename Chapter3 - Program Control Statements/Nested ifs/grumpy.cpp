00e5f525508cbdfb7a973512e247f12a24a8dc79
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-02 15:24:44 +0800

    Committed 2023/08/02

---
+++ Chapter3 - Program Control Statements/Nested ifs/grumpy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i, j, k, result;
+
+	i = 6;
+	j = 1;
+	k = 5;
+
+	if (i) {
+		if (j) result = 1;
+		if (k) result = 2;
+		else result = 3;   // this else is associated with if(k)
+	}
+	else
+		result = 4;        // this else is associated with if(i)
+
+}
