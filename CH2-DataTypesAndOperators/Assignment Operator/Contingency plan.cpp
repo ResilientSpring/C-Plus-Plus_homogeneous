475cc364916be4bec4ee8a021a84e121f49d3b5f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-02 13:12:26 +0800

    Committed 2023/08/02

---
+++ CH2-DataTypesAndOperators/Assignment Operator/Contingency plan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x, y, z;
+
+	x = y = z = 100;
+
+	cout << "x is " << x << "\n";
+	cout << "y is " << y << "\n";
+	cout << "z is " << z << "\n";
+
+}
