178962aa689ba4a850b34a1f9a83ba68ec52d019
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-03 23:07:38 +0800

    Committed 2023/08/03

---
+++ Chapter3 - Program Control Statements/The switch Statement (cases' empty body)/grain.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int input;
+
+	cout << "Enter a number between 1 and 3, inclusive.\n";
+
+	cin >> input;
+
+	switch (input)
+	{
+	case 1:
+		
+	case 2:
+		
+	case 3:
+		
+		cout << "input is less than 4";
+
+	case 4:
+		cout << "input is 4.";
+	}
+}
