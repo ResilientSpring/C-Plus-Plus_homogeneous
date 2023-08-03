e7380bdb3492248f8afdb876c5a62bbd5d70fe5a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-03 12:16:14 +0800

    Committed 2023/08/03

--- Chapter3 - Program Control Statements/The switch Statement/use up something.cpp
+++ Chapter3 - Program Control Statements/The switch Statement/use up something.cpp
@@ -13,6 +13,21 @@ int main() {
 	cout << "Enter a number from 1 to 3: ";
 	cin >> number;
 
-
+	switch (number)
+	{
+	case 1:
+		cout << "A rolling stone gathers no moss. \n";
+		break;
+	case 2:
+		cout << "A bird in the hand is worth two in the bush. \n";
+		break;
+	case 3:
+		cout << "A fool and his money are soon parted. \n";
+		break;
+	default:
+		cout << "You must enter a number between 1 and 3, inclusive. \n";
+		cout << "You must enter either 1, 2, or 3. \n";
+		break;
+	}
 
 }
