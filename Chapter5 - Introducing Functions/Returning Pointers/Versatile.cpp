ebdcfe7baed3720e4a339b3962c62c3800c87daa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-10 16:23:42 +0800

    Committed on or around 2023/08/10

---
+++ Chapter5 - Introducing Functions/Returning Pointers/Versatile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+char *get_substring(char *substring, char *string);
+
+int main() {
+
+
+
+}
+
+
+char *get_substring(char *substring, char *string) {
+
+
+
+}
