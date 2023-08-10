6599f240f8bab2919decd16478a1754e41cf7586
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-10 19:48:24 +0800

    Committed on or around 2023/08/10

--- Chapter5 - Introducing Functions/Test char string/Dividend.cpp
+++ Chapter5 - Introducing Functions/Test char string/Dividend.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main() {
 
-	char *string = "character string";
+//	char *string = "character string";
 
 	const char *string = "Character string";
 
+	cout << string[0] << endl;
 }
