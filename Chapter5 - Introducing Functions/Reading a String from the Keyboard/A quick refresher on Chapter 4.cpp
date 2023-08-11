7785f5fa6b6fa8ba4ffcb46d6a4145f70688a5cb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-11 12:43:52 +0800

    Committed on or around 2023/08/11

---
+++ Chapter5 - Introducing Functions/Reading a String from the Keyboard/A quick refresher on Chapter 4.cpp
@@ -0,0 +1,13 @@
+// Using cin to read a string from the keyboard.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char str[80];
+
+	cout << "Enter a string: ";
+	cin >> str;
+	cout << "Here is your string: " << str;
+
+}
