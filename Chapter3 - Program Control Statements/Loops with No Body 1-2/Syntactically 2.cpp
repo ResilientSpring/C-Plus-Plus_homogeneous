c8a1c0d739e7911c30011ab10544c4805ab63283
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-13 12:03:35 +0800

    Committed on or around 2023/08/13

---
+++ Chapter3 - Program Control Statements/Loops with No Body 1-2/Syntactically 2.cpp
@@ -0,0 +1,22 @@
+// The body of a for loop can be empty.
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+	int i;
+	int sum = 0;
+
+	// sum the numbers from 1 through 10
+	for (i = 1; i <= 10; sum += i++) {
+
+		sum = sum + i;
+
+		i++;
+
+	}
+
+	cout << "Sum is " << sum;
+}
