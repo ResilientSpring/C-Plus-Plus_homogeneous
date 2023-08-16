8cdb576c21c4a5ea6e4a4423936aababd53aea24
Ceiling_roof <ted10014@hotmail.com>
2023-08-16 13:43:05 +0800

    Committed on or around 2023/08/16

---
+++ Chapter 9 - A Closer Look at Classes/Function returning objects 4/take hold of sth or someone.cpp
@@ -0,0 +1,78 @@
+// Returning objects.
+
+#include <iostream>
+using namespace std;
+
+class MyClass {
+
+	int value;
+
+public:
+
+	MyClass(int value) {
+		this.value = value;
+		cout << "Inside constructor\n";
+	}
+
+
+	~MyClass() {
+		cout << "Destructing\n";
+	}
+
+
+	int getValue() {
+
+		return value;
+
+	}
+
+	// Return an object.
+	MyClass mkBigger() {
+
+		MyClass o = MyClass(value * 2);
+
+		return o;
+	}
+};
+
+
+void display(MyClass ob) {
+
+	cout << ob.getValue() << endl;
+
+}
+
+
+int main() {
+
+	cout << "Before constructing a.\n";
+
+	MyClass a = MyClass(10);
+
+	cout << "After constructing a. \n\n";
+
+	///////////////////////////////////////
+
+	cout << "Before call to display(). \n";
+
+	display(a);
+
+	cout << "After display() returns. \n\n";
+
+	////////////////////////////////////////
+
+	cout << "Before call to mkBigger(). \n";
+
+	a = a.mkBigger();
+
+	cout << "After mkBigger() returns. \n\n";
+
+	/////////////////////////////////////////
+
+	cout << "Before the second call to display(). \n";
+
+	display(a);
+
+	cout << "After display() returns. \n\n";
+
+}
