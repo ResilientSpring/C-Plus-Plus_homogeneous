7a18057b15dcb16ecd8b4ea9a7fc6a57b13f6bf5
Ceiling_roof <ted10014@hotmail.com>
2023-08-17 13:49:53 +0800

    Committed on or around 2023/08/17

--- Chapter 9 - A Closer Look at Classes/Overloading Constructors/per se.cpp
+++ Chapter 9 - A Closer Look at Classes/Overloading Constructors/per se.cpp
@@ -23,6 +23,12 @@ public:
 
 int main() {
 
+	Sample t;
+	Sample t1(5);
+	Sample t2(9, 10);
 
+	cout << "t.x: " << t.x << ", t.y: " << t.y << "\n";
+	cout << "t1.x: " << t1.x << ", t1.y: " << t1.y << "\n";
+	cout << "t2.x: " << t2.x << ", t2.y: " << t2.y << "\n";
 
 }
