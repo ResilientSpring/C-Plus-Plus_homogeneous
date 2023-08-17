3a1ccc4f4bdb3e965ac2edaa4a71fb5880e499e1
Ceiling_roof <ted10014@hotmail.com>
2023-08-17 13:35:55 +0800

    Committed on or around 2023/08/17

--- Chapter 9 - A Closer Look at Classes/Overloading Constructors/per se.cpp
+++ Chapter 9 - A Closer Look at Classes/Overloading Constructors/per se.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
+class Sample {
+
+public:
+	int x;
+	int y;
+
+	Sample() { x = y = 0; };     // Overload the default constructor.
+
+};
+
+
 int main() {
 
 
