a203d6c5f56bc96a117ea64265dbc3c1cce166d9
Ceiling_roof <ted10014@hotmail.com>
2023-08-18 12:26:00 +0800

    Committed on or around 2023/08/18

---
+++ Chapter5 - Introducing Functions/Passing Strings/per se = by itself.cpp
@@ -0,0 +1,28 @@
+// Pass a string to a function.
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+int main() {
+
+	char str[80];
+
+	strcpy(str, "This Is A Test");
+
+}
+
+
+void strInvertCase(char *str) {
+
+	while (*str)
+	{
+
+		// invert case
+		if (isupper(*str))
+			*str = tolower(*str);
+
+	}
+
+}
