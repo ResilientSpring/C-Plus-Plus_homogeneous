4048440653a786d0a1c3b4dd4ae516d500d6118a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-18 16:48:55 +0800

    Committed on or around 2023/08/16

---
+++ Chapter3 - Program Control Statements/Do-while Loop 2/Magic Number program.cpp
@@ -0,0 +1,37 @@
+// Magic Number program: 3rd improvement.
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+	int magic;
+	int guess;
+
+	magic = rand();
+
+	do
+	{
+		cout << "Enter your guess: ";
+		cin >> guess;
+
+		if (guess == magic) {
+
+			cout << " ** Right ** ";
+
+			cout << magic << " is the magic number.\n";
+		}
+		else
+		{
+			cout << "...Sorry, you're wrong.";
+
+			if (guess > magic)
+				cout << " Your guess is too high.\n";
+			else
+				cout << " You guess is too low.\n";
+		}
+
+	} while (true);
+
+}
