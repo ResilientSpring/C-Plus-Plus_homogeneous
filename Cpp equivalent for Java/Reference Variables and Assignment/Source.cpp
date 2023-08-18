b28bc09e09aaf45ee5a71f779f20c1531ee1b09b
Ceiling_roof <ted10014@hotmail.com>
2023-08-18 23:43:50 +0800

    Committed on or around 2023/08/18

--- Cpp equivalent for Java/Reference Variables and Assignment/Source.cpp
+++ Cpp equivalent for Java/Reference Variables and Assignment/Source.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 class Vehicle {
 
+public:
 
+	int passengers;
+	int fuelcap;
+	int mpg;
 
 };
 
 
 int main() {
 
+	Vehicle car1 = Vehicle();
+	Vehicle car2;
+
+	car2 = car1;
+
+	car1.mpg = 26;
 
 }
