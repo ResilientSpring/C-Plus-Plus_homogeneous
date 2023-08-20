4f2639066bc9ed869a1d3270f1c963c489afb8ef
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-20 18:12:46 +0800

    Committed on or around 2023/08/20

--- Chapter 9 - A Closer Look at Classes/Passing Objects by Reference/contiguous.cpp
+++ Chapter 9 - A Closer Look at Classes/Passing Objects by Reference/contiguous.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 using namespace std;
 
+
+class MyClass {
+	int value;
+
+public:
+	MyClass(int value) {
+		this->value = value;
+
+		cout << "Inside constructor" << endl;
+	}
+};
+
+
 int main() {
 
 
