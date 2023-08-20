d0302c7021cb5c31b45b828cabfee1095d4afde2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-20 20:01:09 +0800

    Committed on or around 2023/08/20

--- Chapter 9 - A Closer Look at Classes/Constructors, Destructors, and Passing Objects2/contingency allowance 2.cpp
+++ Chapter 9 - A Closer Look at Classes/Constructors, Destructors, and Passing Objects2/contingency allowance 2.cpp
@@ -35,18 +35,22 @@ void display(MyClass ob) {
 
 	cout << ob.get_value() << endl;
 
-	// When the function terminates and the copy of the object usedd as an argument is destroyed, the 
+	// When the function terminates and the copy of the object used as an argument is destroyed, the 
 	// destructor function is called.  This is necessary because the object has gone out of scope.
 }
 
 
+// There will be two calls to the destructor of MyClass's instance.
 int main() {
 
 	MyClass a = MyClass(10);
 
 	cout << "Before calling display(). " << endl;
 
+	// The first is when the parameter to display() goes out of scope.
 	display(a);
 
 	cout << "After display() returns. " << endl;
+
+	// The second is when "a" inside main() is destroyed when the program ends.
 }
