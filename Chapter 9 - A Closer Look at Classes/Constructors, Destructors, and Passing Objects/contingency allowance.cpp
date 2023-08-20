92abf15d71643a178ae61d59da28b5498614e451
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-20 19:01:09 +0800

    Committed on or around 2023/08/20

---
+++ Chapter 9 - A Closer Look at Classes/Constructors, Destructors, and Passing Objects/contingency allowance.cpp
@@ -0,0 +1,46 @@
+// Constructors, destructors, and passing objects.
+
+#include <iostream>
+using namespace std;
+
+
+class MyClass {
+
+	int value;
+
+public:
+
+	MyClass(int value) {
+
+		this->value = value;
+
+		cout << "Inside constructor" << endl;
+	}
+
+
+	~MyClass() {
+
+		cout << "Destructing" << endl;
+
+	}
+
+	int get_value() {
+		return value;
+	}
+
+};
+
+
+void display(MyClass ob) {
+
+	cout << ob.get_value() << endl;
+
+}
+
+
+
+int main() {
+
+
+
+}
