1e2b473f0c7f9eb6ce2af3447e2b4aca7cc9e745
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-22 19:53:39 +0800

    Committed on or around 2023/08/22

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/String Constants/unravel = dig out.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const char *ptr;
+
+	ptr = "Pointers add power to C++.\n";  // ptr is assigned the address of this string constant.
+
+}
