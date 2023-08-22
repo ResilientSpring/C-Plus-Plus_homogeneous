ed6dddd16dc45ae3ef99652557b47556a31cf5e3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-22 15:17:13 +0800

    Committed on or around 2023/08/22

--- Chapter6 - A Closer Look at Functions/Returning References 2/Unravel.cpp
+++ Chapter6 - A Closer Look at Functions/Returning References 2/Unravel.cpp
@@ -21,6 +21,14 @@ int main() {
 	change_it(1) = 5298.23;  // change 2nd element.
 	change_it(3) = -98.8;    // change 4th element.
 
+	cout << "Here are the changed values: ";
+	for ( i = 0; i < 5; i++)
+	{
+		cout << values[i] << ' ';
+	}
+
+	cout << endl;
+
 }
 
 // Return a reference.
