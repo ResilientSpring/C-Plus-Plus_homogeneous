b04f8bac49a4254b8517f2394259bb38b09b1503
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-22 13:44:35 +0800

    Committed on or around 2023/08/22

--- Chapter6 - A Closer Look at Functions/Returning References 2/Unravel.cpp
+++ Chapter6 - A Closer Look at Functions/Returning References 2/Unravel.cpp
@@ -1,1 +1,19 @@
 // Return a reference to an array element.
+
+#include <iostream>
+using namespace std;
+
+
+double values[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+
+int main() {
+
+	int i;
+
+	cout << "Here are the original values: ";
+
+	for(i = 0; i < 5; i++)
+		cout <<
+
+}
