b62a80dc692ccd353c242b8fa0e611d7c31c2dd4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-22 22:01:36 +0800

    Committed on or around 2023/08/22

--- Chapter4 - Arr, Str, and Pointers (refresher)/String Constants 2/unravel = dig out 2.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/String Constants 2/unravel = dig out 2.cpp
@@ -8,5 +8,8 @@ int main() {
 	ptr = "Pointers add power to C++.\n"; // ptr is assigned the address of this string constant.
 	                                     // A string constant is a set of characters enclosed in double quotes.
 	cout << ptr;
+	// The name of a character array that holds a string can be used in any place that a string constant 
+	// can be used.
 
+	// That is, the memory address of 
 }
