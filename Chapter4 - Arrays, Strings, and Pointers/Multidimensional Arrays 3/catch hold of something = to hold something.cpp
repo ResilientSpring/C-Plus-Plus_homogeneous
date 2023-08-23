448fc19dd3284fbc625c09263d85bad807102a00
Ceiling_roof <ted10014@hotmail.com>
2023-08-23 18:05:47 +0800

    committed on or around 2023/08/23

--- Chapter4 - Arrays, Strings, and Pointers/Multidimensional Arrays 3/catch hold of something = to hold something.cpp
+++ Chapter4 - Arrays, Strings, and Pointers/Multidimensional Arrays 3/catch hold of something = to hold something.cpp
@@ -6,5 +6,16 @@ int main() {
 	char multidimensional_array[10][6][4][9];
 
 
-	multidimensional_array[0][0][0][0] = 't';
+	multidimensional_array[0][0][0][0] = 'g';
+	multidimensional_array[0][0][0][1] = 'r';
+	multidimensional_array[0][0][0][2] = 'a';
+	multidimensional_array[0][0][0][3] = 'b';
+	multidimensional_array[0][0][0][4] = ' ';
+
+	multidimensional_array[0][0][1][0] = 'h';
+	multidimensional_array[0][0][1][1] = 'o';
+	multidimensional_array[0][0][1][2] = 'l';
+	multidimensional_array[0][0][1][3] = 'd';
+//	multidimensional_array[0][0][1][4] = '';
+	multidimensional_array[0][0][1][4] = ' ';
 }
