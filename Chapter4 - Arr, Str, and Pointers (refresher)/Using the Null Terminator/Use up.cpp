2637677f898234aeeee2081912a905641c61097b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-23 23:52:13 +0800

    Committed on or around 2023/08/23

--- Chapter4 - Arr, Str, and Pointers (refresher)/Using the Null Terminator/Use up.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Using the Null Terminator/Use up.cpp
@@ -15,7 +15,7 @@ int main() {
 
 	// This loop stops when the null terminator is indexed, because all character values are non-zero, 
 	// but the null, which terminates the string, is zero.
-	for (i = 0; str[i]; i++)  
+	for (i = 0; str[i]; i++)         // True is any non-zero value.
 		str[i] = toupper(str[i]);
 
 	cout << str;
