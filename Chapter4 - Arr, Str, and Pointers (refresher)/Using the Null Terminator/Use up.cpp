0d8fdc9a3fcef55f96469b83fd21085286aee0fc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-24 14:27:38 +0800

    Committed on or around 2023/08/24

--- Chapter4 - Arr, Str, and Pointers (refresher)/Using the Null Terminator/Use up.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Using the Null Terminator/Use up.cpp
@@ -11,13 +11,13 @@ int main() {
 	char str[80];
 	int i;
 
-	// It's not necessary to manually add the null terminator onto the end of string constants; the C++
+	// It's not necessary to manually add the null (terminator) onto the end of string constants; the C++
 	// compiler does this for you. Therefore, the string "this is a test" will appear in memory like this:
 	// t h i s  i s  a  t e s t 0
 	// 0 1 2 3 4 ...............14
 	strcpy(str, "this is a test");
 
-	// This loop stops when the null terminator is indexed, because all character values are non-zero, 
+	// This loop stops when the null (ASCII = 0) is reached, because all character values are non-zero, 
 	// but the null, which terminates the string, is zero.
 	for (i = 0; str[i]; i++)         // True is any non-zero value.
 		str[i] = toupper(str[i]);
