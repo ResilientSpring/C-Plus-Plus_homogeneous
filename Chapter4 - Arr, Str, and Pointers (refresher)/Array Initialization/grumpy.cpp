47e97a2f0109bf61e03cedd366517ea166b6e969
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-25 12:17:36 +0800

    Committed on or around 2023/08/25

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Array Initialization/grumpy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	// A character array (an array of character) that will hold a string allows a shorthand initialization
+	// when it is declared.
+	char str[4] = "C++";
+
+}
