2faa42922ef81e4e8fcc2f1f3dc3df3a4927daa7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-25 13:33:11 +0800

    Committed on or around 2023/08/25

--- Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Strings/leftmost.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Strings/leftmost.cpp
@@ -1,10 +1,19 @@
 // A simple computerized telephone directory.
-
+#include <cstdio>
 #include <iostream>
 using namespace std;
 
 int main() {
 
 	char str_array[30][80];
+
+	int i;
+	char str[80];
+	char numbers[10][80] = {
+
+		"Tom",
+		"555-3322"
+
+	};
 
 }
