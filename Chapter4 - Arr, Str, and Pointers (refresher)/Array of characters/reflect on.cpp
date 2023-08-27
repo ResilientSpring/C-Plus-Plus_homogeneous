1ed3b7c2f0f4c11489306f032848efaf831cbaa3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 23:37:16 +0800

    Committed on or around 2023/08/27

--- Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
@@ -14,7 +14,7 @@ int main() {
 	cout << array_of_characters[4] << endl;
 	cout << array_of_characters[5] << endl;
 
-	cout << array_of_characters[6] << " " << array_of_characters[7] << array_of_characters[8] << " ";
+	cout << array_of_characters[6] << " " << array_of_characters[7] << array_of_characters[8];
 	cout << array_of_characters[9] << " " << array_of_characters[10] << array_of_characters[11] << endl;
 
 	cout << array_of_strings[0][1] << array_of_strings[0][7] << array_of_strings[1][8] << endl;
