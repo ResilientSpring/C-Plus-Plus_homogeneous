9fa36daf096b89c657768421d7fe03e8c7a6e97e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 23:35:31 +0800

    Committed on or around 2023/08/27

--- Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
@@ -5,7 +5,7 @@ int main() {
 
 	char array_of_characters[] = "access one character among the charactres.";
 	char array_of_strings[][80] = { "string one", "string two", "string three" };
-	                           // { {'s', 't', 'r', 'i', 'n', 'g', ' ', 'o', 'n', 'e'}, {'s', 't', 'r', 'i'} }
+	char array_of_string_[][80] = { {'s', 't', 'r', 'i', 'n', 'g', ' ', 'o', 'n', 'e'}, {'s', 't', 'r', 'i'} };
 
 	cout << array_of_characters[0] << endl;
 	cout << array_of_characters[1] << endl;
@@ -17,5 +17,5 @@ int main() {
 	cout << array_of_characters[6] << " " << array_of_characters[7] << array_of_characters[8] << " ";
 	cout << array_of_characters[9] << " " << array_of_characters[10] << array_of_characters[11] << endl;
 
-
+	cout << array_of_strings[0][1] << array_of_strings[0][7] << array_of_strings[1][8] << endl;
 }
