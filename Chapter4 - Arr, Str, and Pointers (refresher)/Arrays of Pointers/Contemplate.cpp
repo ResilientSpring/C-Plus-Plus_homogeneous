23781ce3997a17d5c0b9860947990415c767784d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 15:56:05 +0800

    Committed on or around 2023/08/27

--- Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers/Contemplate.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers/Contemplate.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main() {
 
+	// The general form of a one-dimensional array declaration is: type name[size];
 	int *pi[10];
 
-	int var;
+	int var = 8;
+
+	// ----- End of declaration statements.
 
 	pi[2] = &var;
 
-	cout << *pi[2];
+	cout << *pi[2];  // * is an operator representing the value at the memory address of its operand. 
 
 }
