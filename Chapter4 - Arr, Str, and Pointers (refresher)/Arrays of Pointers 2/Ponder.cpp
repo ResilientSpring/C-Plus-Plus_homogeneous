59b2ec77e5b64965b43b571f9da36e0928af6740
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 16:09:20 +0800

    Committed on or around 2023/08/27

--- Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers 2/Ponder.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers 2/Ponder.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int main() {
 
 	const char *dictionary[][2] = {
-	
-		"pencil", "A writing instrument.", 
-		"keyboard", "An input device.",
 
+		"pencil", "A writing instrument.",
+		"keyboard", "An input device.",
+		"rifle", "A shoulder-fired firearm.",
+		"airplane", "A fixed-wing aircraft.",
+		"network", "An interconnected group at computers.",
+		"", ""
 	
 	};
 
