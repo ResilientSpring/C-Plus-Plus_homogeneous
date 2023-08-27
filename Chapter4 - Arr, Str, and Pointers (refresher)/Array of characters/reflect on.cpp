463d21b97757ebbfd8d651757fb9e4a4a0b38211
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 23:21:48 +0800

    Committed on or around 2023/08/27

--- Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Array of characters/reflect on.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
 
 	char array_of_characters[] = "access one character among the charactres.";
-	char array_of_strings[][] = { "string one", "string two", "string three" };
+	char array_of_strings[][80] = { "string one", "string two", "string three" };
 
 }
