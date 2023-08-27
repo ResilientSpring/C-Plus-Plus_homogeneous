4ff145306fecb7c77d2943f2957004e806cecdb5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-27 16:07:09 +0800

    Committed on or around 2023/08/27

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers 2/Ponder.cpp
@@ -0,0 +1,16 @@
+// Use a 2-D array of pointers to create a dictionary.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const char *dictionary[][2] = {
+	
+		"pencil", "A writing instrument.", 
+		"keyboard", "An input device.",
+
+	
+	};
+
+}
