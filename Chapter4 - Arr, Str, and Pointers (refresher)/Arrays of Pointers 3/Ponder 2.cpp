9570e2d620c9adcbb1e1c0409c73d0ad86e5b2da
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-28 14:49:32 +0800

    Committed on or around 2023/08/28

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Pointers 3/Ponder 2.cpp
@@ -0,0 +1,52 @@
+// Use a 2-D array of pointers to create a dictionary.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// The general form of a one-dimensional array declaration is: type name[size];
+	const char* dictionary[][2] = {
+
+		"pencil", "A writing instrument.",
+		"keyboard", "An input device.",
+		"rifle", "A shoulder-fired firearm.",
+		"airplane", "A fixed-wing aircraft.",
+		"network", "An interconnected group of computers.",
+		"", ""
+
+	};
+
+	/*
+	
+	const char* dictionary[][2] = {
+	
+	    pointer, pointer
+	    pointer, pointer
+	       :   ,   :
+		   :   ,   :
+
+	}
+	
+	*/
+
+
+
+	char word[80];
+	int i;
+	cout << "Enter word: ";
+	cin >> word;
+
+	for (i = 0; *dictionary[i][0]; i++) {
+		if (!strcmp(dictionary[i][0], word)) {
+
+			cout << dictionary[i][1] << endl;
+
+			break; // When the break statement is encountered inside a loop, the loop is immediately terminated, 
+				   // and program control resumes at the next statement following the loop.
+		}
+	}
+
+	if (!*dictionary[i][0])
+		cout << word << " not found. " << endl;
+}
