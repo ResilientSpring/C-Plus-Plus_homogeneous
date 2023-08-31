214a4c89380b3c42fa6d41f5a0526aaa19c51486
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-31 22:08:56 +0800

    Committed on or around 2023/08/31

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/ExceptionThrownBy func/backdoor.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/ExceptionThrownBy func/backdoor.cpp
@@ -5,6 +5,15 @@ Throwing an exception from a function called from within a try block.
 #include <iostream>
 using namespace std;
 
+void Xtest(int test) {
+
+	cout << "Inside Xtest, test is: " << test << endl;
+
+	if (test)
+		throw test;
+}
+
+
 int main() {
 
 	cout << "start\n";
