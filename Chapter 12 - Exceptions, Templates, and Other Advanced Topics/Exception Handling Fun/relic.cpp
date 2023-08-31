1e26e5b03a455191ba37639f3022e5c2fc011f7b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-31 11:46:03 +0800

    Committed on or around 2023/08/31

--- Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Exception Handling Fun/relic.cpp
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Exception Handling Fun/relic.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main() {
 
+	cout << "start\n";
 
+	// Program statements that you want to monitor for exceptions are contained in a try block.
+	try
+	{
+
+	}
+	catch (const std::exception &)
+	{
+
+	}
 
 }
