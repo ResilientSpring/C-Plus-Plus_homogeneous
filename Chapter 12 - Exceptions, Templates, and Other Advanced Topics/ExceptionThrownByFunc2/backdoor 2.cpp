83e1d8b85c21bb90d46caa273b09e9dfe20194f5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-01 13:21:37 +0800

    Committed on or around 2023/09/01

---
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/ExceptionThrownByFunc2/backdoor 2.cpp
@@ -0,0 +1,36 @@
+/*
+Throwing an exception from a function called from within a try block.
+*/
+
+#include <iostream>
+using namespace std;
+
+void Xtest(int test) {
+
+	cout << "Inside Xtest, test is: " << test << endl;
+
+	if (test)
+		throw test;  // This thrown exception is caught by the catch statement in main(), even if 
+	                 // this function's return type is void. 
+}
+
+
+int main() {
+
+	cout << "start\n";
+
+	// Program statements that you want to monitor for errors are contained in a try block.
+	try
+	{
+		cout << "Inside try block\n";
+		Xtest(0);
+		Xtest(1); // Because Xtest() is called from within a try block, its code is also monitored for errors.
+		Xtest(2);
+	}
+	catch (int i)
+	{
+		cout << "Caught an exception -- value is: " << i << endl;
+	}
+
+	cout << "end";
+}
