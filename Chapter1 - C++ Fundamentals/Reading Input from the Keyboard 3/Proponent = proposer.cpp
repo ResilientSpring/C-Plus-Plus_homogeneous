beadf7d0ec1aca3f917a0f9e902332b758049e48
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-03 13:46:51 +0800

    Committed on or around 2023/09/03

---
+++ Chapter1 - C++ Fundamentals/Reading Input from the Keyboard 3/Proponent = proposer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int length;  // this declares a variable
+	int width;
+
+	do
+	{
+		cout << "Enter the length: ";
+		cin >> length;
+
+	} while (length != );
+
+	for (; ;) {
+
+		cout << "Enter the length: ";
+		cin >> length;
+
+		cout << "Enter the width: ";
+		cin >> width;
+
+	}
+
+
+	cout << "The area is " << length * width << endl;
+
+}
