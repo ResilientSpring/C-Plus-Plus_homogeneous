a8c37a2158b9aac336482aeae34ce73cf88d7880
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-05 23:11:42 +0800

    Committed on or around 2023/09/05

--- Chapter4 - Arr, Str, and Pointers (refresher)/The Null Pointer Convention/reckon = feel.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/The Null Pointer Convention/reckon = feel.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main() {
 
 	// Any type of pointer can be initialized to null when it is declared.
+	float *p = 0;  // p is now a null pointer
+
+	// To check for a null pointer, use an if statement:
+	if (p) {
+
+		cout << "p is not null" << endl;
+
+	}
+
+	if (!p) {
+
+		cout << "p is null" << endl;
+	}
 
 }
