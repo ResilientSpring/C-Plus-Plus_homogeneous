81c55ddf9b16b3dc88c502008fa845ce7c63237a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-05 14:51:06 +0800

    Committed on or around 2023/09/05

--- Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Strings 2/implicate.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Arrays of Strings 2/implicate.cpp
@@ -10,4 +10,10 @@ int main() {
 	char arr_str2[5][10] = { "implicate", "implicit", "hedge", "possibly", "abc"};
 
 	// This is an array of 5 strings, each can hold 9 characters.
+
+
+	cout << str << endl;
+	cout << arr_str[0] << endl;
+	cout << arr_str[0][1] << endl;
+	cout << arr_str[1][2] << endl;
 }
