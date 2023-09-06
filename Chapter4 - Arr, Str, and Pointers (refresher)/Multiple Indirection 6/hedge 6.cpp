97db9a52c6b933c50bf89d6b0a93352646207252
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-07 00:57:17 +0800

    Committed on or around 2023/09/07

--- Chapter4 - Arr, Str, and Pointers (refresher)/Multiple Indirection 6/hedge 6.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Multiple Indirection 6/hedge 6.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main() {
 
-	// In a declaration statement, preceding a variable name with ** causes that variable to be 
-	// a pointer to a pointer.
-	int x, *p, **q;
+	// In a declaration statement,  
+
+	int x;
+
+	// preceding a variable name with a *causes that variable to be a pointer.
+	int *p;  // A pointer is an object that can contain a memory address.
+
+	// preceding a variable name with ** causes that variable to be a pointer to a pointer.
+	int **q;
 
 	// ----- End of declaration statements.
 
