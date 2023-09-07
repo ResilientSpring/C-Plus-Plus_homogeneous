d212848623a3f28f3ac02c225b7bdd8301cbd3bc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-07 14:18:29 +0800

    Committed on or around 2023/09/07

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Multiple Indirection 8/hedge 8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// In a declaration statement,  
+
+	int x;
+
+	// preceding a variable name with a * causes that variable to be a pointer.
+
+	int *p;  // A pointer is a variable that can hold the memory address of another variable.
+
+
+	// preceding a variable name with ** causes that variable to be a pointer to a pointer.
+
+	int **q;  // A pointer to a pointer is a variable that can hold the memory address of another pointer.
+
+
+	// ----- End of declaration statements.
+
+	x = 10;
+
+	p = &x;  // get address of total.  & is an operator standing for the memory address of its operand.
+
+	q = &p;
+
+	cout << *q << endl;  // * is an operator representing the value at the memory address of its operand.
+
+	cout << **q << endl;
+
+	cout << *(*q) << endl;
+}
