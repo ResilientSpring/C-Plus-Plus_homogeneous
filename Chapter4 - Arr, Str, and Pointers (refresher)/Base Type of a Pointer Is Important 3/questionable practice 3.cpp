704d3e1681f4f14fd4b85ee7440ac1017d434760
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-12 15:06:58 +0800

    Committed on or around 2023/09/12

--- Chapter4 - Arr, Str, and Pointers (refresher)/Base Type of a Pointer Is Important 3/questionable practice 3.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Base Type of a Pointer Is Important 3/questionable practice 3.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
 
 	int x = 123;
-	double y, *p = (double *) &x;   // Use cast to assign int * to double * ;
+	double y, *p;
+
+	p = (double *) &x;   // Use cast to assign int * to double * ;
 
 	y = *p;      // What will this do?
 	cout << y;   // What will this print?
