1622fb36eadebd08579b8bb281c4be6c965e110e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-15 22:26:29 +0800

    Committed on or around 2023/09/15

---
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Cast Pointer Type 2/nonsensical = nonsense.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "sizes: double = " << sizeof(double);
+	cout << ", long = " << sizeof(long);
+	cout << ", int = " << sizeof(int);
+	cout << ", char = " << sizeof(char);
+
+}
