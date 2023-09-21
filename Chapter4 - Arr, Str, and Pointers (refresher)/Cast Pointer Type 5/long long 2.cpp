5426a382c9a691db4774545e19a28059606ebbf7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-21 14:41:19 +0800

    Committed on or around 2023/09/21

--- Chapter4 - Arr, Str, and Pointers (refresher)/Cast Pointer Type 5/long long 2.cpp
+++ Chapter4 - Arr, Str, and Pointers (refresher)/Cast Pointer Type 5/long long 2.cpp
@@ -9,17 +9,32 @@ int main() {
 	cout << ", char = " << sizeof(char) << endl;
 
 	double d;
-	double *dp = &d;
-	long long *lp = (long long *)&d;
-	int *ip = (int *)&d;
-	char *cp = (char *)&d;
+	double *dp;
+	long long *lp;
+	int *ip;
+	char *cp;
 
-	//	printf("dp == %p, lp = %p\nip == %p, cp == %p\n\n", dp, lp, ip, cp);
+	dp = &d;
+	lp = (long long *)&d;
+	ip = (int *)&d;
+	cp = (char *)&d;
+
+	printf("dp == %p, lp == %p\nip == %p, cp == %p\n\n", dp, lp, ip, cp);
+
 
 	cout << "dp == " << dp << ", lp = " << lp << endl;
-	cout << "ip == " << ip << ", cp = " << cp << "\n\n";
+	cout << "ip == " << ip << ", cp = " << cp << "\n\n";  // Print memory address of a string of characters.
 
 	d = 42.0;
 	printf("*dp == %.20f, *lp == %ld, *ip == %d, *cp == %d\n", *dp, *lp, *ip, *cp);
-	cout << "*dp == " << *dp << ", *lp == " << *lp << ", *ip == " << *ip << ", *cp == " << *cp << endl;
+	cout << "*dp == " << *dp << ", *lp == " << *lp << ", *ip == " << *ip << ", *cp == " << *cp << "\n\n";
+
+	*ip = 4200;
+	printf("*dp == %.20f, *lp == %ld, *ip == %d, *cp == %d\n", *dp, *lp, *ip, *cp);
+	cout << "*dp == " << *dp << ", *lp == " << *lp << ", *ip == " << *ip << ", *cp == " << *cp << "\n\n";
+
+	*cp = 42;
+	printf("*dp == %.20f, *lp == %ld, *ip == %d, *cp == %d\n", *dp, *lp, *ip, *cp);
+	cout << "*dp == " << *dp << ", *lp == " << *lp << ", *ip == " << *ip << ", *cp == " << *cp << "\n\n";
+
 }
