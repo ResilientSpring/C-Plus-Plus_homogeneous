9cf28a54b1a37bcdf7abba6977079ffedca5d573
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-17 15:58:55 +0800

    Committed on or around 2023/10/17

---
+++ CH33-Working with STL Containers/Priority queue (CppReference for C++11)/C++11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+	priority_queue<int> priority_queue_of_integers;
+
+	for (int n : {1, 8, 5, 6, 3, 4, 0, 9, 3, 2})
+	{
+
+	}
+
+}
