d351c7d2e948c2a3ce49935b05b3997a46fe9a1b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-18 12:49:01 +0800

    Committed on or around 2023/10/18

--- CH44-Algorithms/Create and Manage a Heap/Interim.cpp
+++ CH44-Algorithms/Create and Manage a Heap/Interim.cpp
@@ -5,16 +5,17 @@
 #include <algorithm>
 using namespace std;
 
+void show(const char *message, vector<char> vect);
+
 int main() {
 
 	vector<char> v;
 
 	for (int i = 0; i < 20; i+=2)
-	{
 		v.push_back('A' + i);
-	}
-
-
+	
+	show("v before building heap: \n", v);
+	cout << endl;
 
 }
 
