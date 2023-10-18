d3ff84db581838de0d1a4d70e4a4e9eedf93d6c5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-18 14:05:35 +0800

    Committed on or around 2023/10/18

---
+++ CH44-Algorithms/Create and Manage a Heap (annotated)/heap.cpp
@@ -0,0 +1,46 @@
+// Demonstrate the heap algorithms.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void show(const char *message, vector<char> vect);
+
+int main() {
+
+	vector<char> v;
+
+	for (int i = 0; i < 20; i += 2)
+		v.push_back('A' + i);
+
+	show("v before building heap: \n", v);
+	cout << endl;
+
+	// Construct a heap.
+	make_heap(v.begin(), v.end());  // specify the range of elements to be made into a heap.
+
+	show("v after building heap: \n", v);
+	cout << endl;
+
+	// Push H onto heap.
+	v.push_back('H');                // first, put H into vector.
+
+	push_heap(v.begin(), v.end());   // Now, push H onto heap.
+
+
+	show("v after pushing H onto heap: \n", v);
+	cout << endl;
+}
+
+
+// Display the contents of a vector<char>
+void show(const char *message, vector<char> vect) {
+
+	cout << message;
+
+	for (unsigned i = 0; i < vect.size(); ++i)
+		cout << vect[i] << " ";
+
+	cout << "\n";
+}
