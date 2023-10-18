9ea6f04e2bc0092c7cb53f5736bf97127d475d53
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-18 19:09:29 +0800

    Committed on or around 2023/10/18

--- CH44-Algorithms/Create and Manage a Heap 4/push_heap() 2.cpp
+++ CH44-Algorithms/Create and Manage a Heap 4/push_heap() 2.cpp
@@ -59,7 +59,7 @@ int main() {
 
 	// Sort the heap into ascending order
 	sort_heap(v.begin(), v.end() - 1);    // v.end() - 1 b/c the element at the end of the container
-	                                      // was popped from the heap.
+	                                      // is the one that was popped from the heap.
 
 	show("v after sorting the heap: \n", v);
 }
