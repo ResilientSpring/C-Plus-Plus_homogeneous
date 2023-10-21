8c68a8cc93b376e69919a5bf964ec2d573630ab8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-21 22:25:31 +0800

    Committed on or around 2023/10/21

---
+++ Assortment/Bubble sort 3/Pass an array, with sized specified.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[]);
+
+int main() {
+
+	int nums[] = { 27, 7, 36, 15, 1, 9 };
+
+	bubble_sort(nums);
+
+	for (int i : nums)
+		cout << i << " ";
+
+}
+
+
+void bubble_sort(int arr[6]) {
+
+	int size = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < size; i++)      
+		for (int j = size - 1; j > i; j--) {
+
+			if (arr[j] < arr[j - 1])
+			{
+				int k = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = k;
+			}
+
+		}
+
+}
