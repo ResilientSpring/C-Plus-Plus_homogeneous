3073be589ec85ce28b392288134d52bbf7a00542
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-02 23:04:09 +0800

    Committed on or around 2023/11/02

---
+++ CH2-DataTypesAndOperators/Type Conversion in Assignments/Octet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x;
+	char ch;
+	float f;
+
+	ch = x;   // line 1
+	x = f;    // line 2
+	f = ch;   // line 3
+	f = x;    // line 4
+
+}
