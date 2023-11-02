8ce104270b447c38483eb4b41b632d5812e204e4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-02 23:11:02 +0800

    Committed on or around 2023/11/02

--- CH2-DataTypesAndOperators/Type Conversion in Assignments 2/lopped off.cpp
+++ CH2-DataTypesAndOperators/Type Conversion in Assignments 2/lopped off.cpp
@@ -7,7 +7,7 @@ int main() {
 	char ch;
 	float f;
 
-	ch = i;   // line 1
+	ch = i;   // line 1    The high-order bits of the integer variable i are lopped off.
 	i = f;    // line 2
 	f = ch;   // line 3
 	f = i;    // line 4
