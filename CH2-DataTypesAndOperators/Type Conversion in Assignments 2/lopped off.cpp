0a6d06bc544222548b88e422a1aa86e3f805b7be
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-02 23:08:33 +0800

    Committed on or around 2023/11/02

---
+++ CH2-DataTypesAndOperators/Type Conversion in Assignments 2/lopped off.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i;
+	char ch;
+	float f;
+
+	ch = i;   // line 1
+	i = f;    // line 2
+	f = ch;   // line 3
+	f = i;    // line 4
+
+}
