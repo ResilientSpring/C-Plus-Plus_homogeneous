a89161f336a3df3de69b81c81160d9a04df1b8d4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-24 23:59:07 +0800

    Committed on or around 2023/11/24

--- Chapter 9 - A Closer Look at Classes/Creating and Using a Copy Constructor/Spoof.cpp
+++ Chapter 9 - A Closer Look at Classes/Creating and Using a Copy Constructor/Spoof.cpp
@@ -7,3 +7,27 @@ Copy constructor invoked when passing an object to a function.
 #include <iostream>
 using namespace std;
 
+class MyClass {
+
+	int val;
+	int copynumber;
+
+public:
+
+	MyClass(int i) {
+		val = i;
+		copynumber = 0;
+		cout << "Inside normal constructor\n";
+	}
+
+	MyClass() {
+
+
+	}
+
+};
+
+
+int main() {
+
+}
