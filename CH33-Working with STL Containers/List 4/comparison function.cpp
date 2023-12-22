8149264d4eb5360f7702387371a8fd8d747f17ce
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-22 12:55:46 +0800

    Committed on or around 2023/12/22

--- CH33-Working with STL Containers/List 4/comparison function.cpp
+++ CH33-Working with STL Containers/List 4/comparison function.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main() {
 
+	list<int>   list_num;
+	list<char>  list_char;
 
+	list_num.push_back(2);
+	list_num.push_back(3);
+	list_num.push_back(1);
+	list_num.push_back(20);
+
+	list_char.push_back('2');
+	list_char.push_back('3');
+	list_char.push_back('1');
+	list_char.push_back('20');
+
+
+
+}
+
+
+bool my_sort() {
 
 }
