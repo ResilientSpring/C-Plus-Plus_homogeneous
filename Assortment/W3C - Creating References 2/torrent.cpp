cfc9f3e8f41fdfc8b9356cf80735b351b9999edc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-30 23:37:00 +0800

    Committed on or around 2023/12/30

--- Assortment/W3C - Creating References 2/torrent.cpp
+++ Assortment/W3C - Creating References 2/torrent.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main() {
 
-	string food = "Pizza";
-	string &meal = food;
+	// A reference variable is a "reference" to an existing variable, and it is created with the & operator:
 
+	string food = "Pizza";  // food variable.
+	string &meal = food;    // reference to food.
+
+	// Now, we can use either the variable name food or the reference name meal to refer to the food variable:
 	cout << food << "\n";
-	cout << meal << "\n";
+	cout << meal << "\n"; 
+
+	// meal is a "reference" to food.
 
 }
