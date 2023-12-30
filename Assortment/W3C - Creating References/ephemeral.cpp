b6f6e13afe9446de2f6f156cd6bf89caca712c61
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-30 23:19:36 +0800

    Committed on or around 2023/12/30

---
+++ Assortment/W3C - Creating References/ephemeral.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	// A reference variable is a "reference" to an existing variable, and it is created with the & operator:
+
+	string food = "Pizza";  // food variable.
+	string &meal = food;   // reference to food.
+
+
+}
