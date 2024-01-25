abcdeef1f5a83dd701341da885c645cd693e38dc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-25 14:24:29 +0800

    Committed on or around 2024/01/25

---
+++ Chapter 9 - A Closer Look at Classes/Passing Objects to Functions 4/clutter 4.cpp
@@ -0,0 +1,35 @@
+// Pass an object to a function.
+
+#include <iostream>
+using namespace std;
+
+class MyClass {
+
+	int value;
+
+public:
+	
+	MyClass(int value) {
+		this->value = value;
+	}
+
+	int get_value() { return value; }
+
+	void set_value(int value) {
+		this->value = value;
+	}
+
+};
+
+
+void display(MyClass object) {    // display() takes a MyClass object as a parameter.
+
+
+}
+
+
+int main() {
+
+	
+
+}
