1167fdd2436fe652f9c48a20e2bc11d79b631707
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-10 16:49:17 +0800

    Committed on or around 2024/02/10

---
+++ Chapter 12 - Exceptions, Templates, and Other Advanced Topics/Dynamic 2D array 2/An array of pointers = arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const int sizeX = 4;
+	int sizeY = 3;
+
+	int(*ary)[sizeX] = new int[sizeY][sizeX];
+
+}
+
+// Reference: https://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-in-c-using-new/936709#comment744797_936687
