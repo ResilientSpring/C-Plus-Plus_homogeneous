fbd9c899d73c45a48c650794f3e9899b04b51f2f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-27 23:25:50 +0800

    Committed on or around 2024/02/27

--- Cpp equivalent for PHP/Retrieving Array Size/map of map.cpp
+++ Cpp equivalent for PHP/Retrieving Array Size/map of map.cpp
@@ -8,6 +8,13 @@ using namespace std;
 int main() {
 
 	
-	map<string, map<string, any>> states;
+	map<string, map<string, any>> states;   // [1][2][3]
 
 }
+
+/*
+* References:
+* 1. https://stackoverflow.com/questions/24702235/c-stdmap-holding-any-type-of-value
+* 2. https://stackoverflow.com/questions/68872633/is-there-a-way-to-store-auto-value-in-c-map
+* 3. https://stackoverflow.com/questions/54668866/visual-studio-2017-cant-find-stdvariant
+*/
