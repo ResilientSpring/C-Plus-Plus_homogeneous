e99c34cc98a9f8e33816e03c3e7874442054c55c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-27 23:35:30 +0800

    Committed on or around 2024/02/27

--- Cpp equivalent for PHP/Retrieving Array Size/map of map.cpp
+++ Cpp equivalent for PHP/Retrieving Array Size/map of map.cpp
@@ -10,7 +10,7 @@ int main() {
 	
 	map<string, map<string, any>> states;   // [1][2][3]
 
-	states["Ohio"]["population"] = 11353140;
+	states["Ohio"]["population"] = 11353140;  // [4]
 
 }
 
@@ -19,4 +19,5 @@ int main() {
 * 1. https://stackoverflow.com/questions/24702235/c-stdmap-holding-any-type-of-value
 * 2. https://stackoverflow.com/questions/68872633/is-there-a-way-to-store-auto-value-in-c-map
 * 3. https://stackoverflow.com/questions/54668866/visual-studio-2017-cant-find-stdvariant
+* 4. https://stackoverflow.com/questions/4479017/storing-std-map-in-map
 */
