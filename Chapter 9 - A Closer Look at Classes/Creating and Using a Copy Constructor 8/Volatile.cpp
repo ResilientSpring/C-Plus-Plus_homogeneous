8282578434406e13bcc7833ff1d7385d0de4692f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-29 00:35:44 +0800

    Committed on or around 2024/02/29

---
+++ Chapter 9 - A Closer Look at Classes/Creating and Using a Copy Constructor 8/Volatile.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+volatile class MyClass {
+
+	int val;
+	int copynumber;
+
+public:
+
+	MyClass(int i) {
+		val = i;
+		copynumber = 0;
+		cout << "Inside normal constructor\n";
+	}
+
+	// Copy constructor Here, obj is a reference to an object that is being used to initialize another object.
+	MyClass(const MyClass& obj) {
+
+		val = obj.val;
+		copynumber = obj.copynumber + 1;
+
+		cout << "Inside copy constructor.\n";
+	}
+
+	~MyClass() {
+
+		if (copynumber == 0)
+			cout << "Destructing original.\n";
+		else
+			cout << "Destructing copy #" << copynumber << endl;
+
+	}
+
+	int getval() {
+		return val;
+	}
+
+	MyClass returns_a_MyClass_object(int value, int copynumber) {
+
+		return MyClass(value);
+
+	}
+
+};
+
+int main() {
+
+	MyClass a = return_a_MyClass_object(5, 6);
+
+	cout << a.getval() << endl;
+
+}
