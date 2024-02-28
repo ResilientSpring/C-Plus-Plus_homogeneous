8d8701a598667308ef0c952a66b7364d90c2f233
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-28 12:51:26 +0800

    Committed on or around 2024/02/28

---
+++ Cpp equivalent for PHP/Retrieving Array Size 2/map of map 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+#include <any>
+using namespace std;
+
+int main() {
+
+
+	map<string, map<string, any>> states;   // [1][2][3]
+
+	states["Ohio"]["population"] = 11353140;  // [4]
+
+//	cout << states["Ohio"]["population"] << endl;
+
+	map<string, map<string, any>>::iterator itr;
+
+	for (itr = states.begin(); itr != states.end(); itr++)
+	{
+		//cout << "Key: " << itr->first << ", Value: " << itr->second << endl; itr->second cotains a map.
+		cout << "Key: " << itr->first << endl;
+	}
+
+}
+
+/*
+* References:
+* 1. https://stackoverflow.com/questions/24702235/c-stdmap-holding-any-type-of-value
+* 2. https://stackoverflow.com/questions/68872633/is-there-a-way-to-store-auto-value-in-c-map
+* 3. https://stackoverflow.com/questions/54668866/visual-studio-2017-cant-find-stdvariant
+* 4. https://stackoverflow.com/questions/4479017/storing-std-map-in-map
+*/
