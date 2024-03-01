92c9767ea05437d058bcedde20aee6b7abc49496
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-02 00:29:27 +0800

    Committed on or around 2024/03/02

--- Chapter 9 - A Closer Look at Classes/Friend functions 4/hordes.cpp
+++ Chapter 9 - A Closer Look at Classes/Friend functions 4/hordes.cpp
@@ -21,7 +21,7 @@ int comDenom(MyClass x) {
 	// Because comDenom() is NOT a friend of MyClass, it can NOT directly access a and b.
 	// int max = x.a < x.b ? x.a : x.b;
 
-	int max = x.i > x.j ? x.i : x.j;
+	int max = x.a > x.b ? x.a : x.b;
 
 	for (int i = 2; i <= max; i++)
 		if ((x.a % i) == 0 && (x.b % i) == 0) return i;
