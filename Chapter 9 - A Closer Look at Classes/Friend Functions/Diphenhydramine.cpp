59fcb8371546e86669b2eea519376b0ffbf2eeac
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-01 15:48:39 +0800

    Committed on or around 2024/03/01

---
+++ Chapter 9 - A Closer Look at Classes/Friend Functions/Diphenhydramine.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+
+	int a, b;
+
+public:
+
+	MyClass(int i, int j) {
+
+		a = i;
+		b = j;
+	}
+
+	friend int comDenom(MyClass x);  // comDenom is a friend of MyClass.
+
+};
+
+// Notice that comDenom() is not a member function of any class.
+int comDenom(MyClass x) {
+
+	// Because comDenom() is a friend of MyClass, it can directly access a and b.
+	int max = x.a < x.b ? x.a : x.b;
+
+}
+
+
+int main() {
+
+	
+
+}
