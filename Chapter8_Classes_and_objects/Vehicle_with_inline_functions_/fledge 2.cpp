768753dc781995a5cb6e80e7855521ff6985fa3e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-08 00:27:12 +0800

    Committed on or around 2024/03/08

---
+++ Chapter8_Classes_and_objects/Vehicle_with_inline_functions_/fledge 2.cpp
@@ -0,0 +1,61 @@
+// Define constructor, destructor, and range() function in-line.
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+// Declare the Vehicle class.
+class Vehicle {
+	// These are now private.
+	int passengers;
+	int fuelcap;
+	int mpg;
+
+public:
+	// This is a constructor for Vehicle.
+	Vehicle(int p, int f, int m) {
+		passengers = p;
+		fuelcap = f;
+		mpg = m;
+	}
+
+	// Compute and return the range.
+	int range() {
+		return mpg * fuelcap;
+	}
+
+	// Accessor functions.
+	int get_passenger() {
+		return passengers;
+	}
+
+	// Define functions inline.
+	int get_fuelcap() {
+		return fuelcap;
+	}
+
+	// Access private variables through public accessor functions.
+	int get_mpg() {
+		return mpg;
+	}
+};
+
+int main() {
+	// Pass value to Vehicle constructor.
+	Vehicle minivan = Vehicle(7, 16, 21);
+	Vehicle sportscar(2, 14, 12);
+
+	Vehicle Porsche = Vehicle(4, 5, 6);
+	Porsche.passengers;
+
+	int range1, range2;
+
+	// Compute the ranges assuming a full tank of gas.
+	range1 = minivan.range();
+	range2 = sportscar.range();
+
+	printf("Minivan can carry %d with a range of %d", minivan.get_passenger(), range1);
+
+	cout << "\nSportscar can carry " << sportscar.get_passenger() << " with a range of " << range2;
+
+	return 0;
+}
