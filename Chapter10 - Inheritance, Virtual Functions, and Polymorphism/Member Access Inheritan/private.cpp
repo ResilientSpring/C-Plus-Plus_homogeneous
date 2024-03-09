35d0f0d0b22b627b46d9c120d19d0ff982f3afa6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 18:29:24 +0800

    Committed on or around 2024/03/09

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Member Access Inheritan/private.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class twoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	void showDim() {
+		std::cout << "Width and height are " << width << " and " << height << "\n";
+	}
+
+};
+
+int main() {
+
+
+
+}
