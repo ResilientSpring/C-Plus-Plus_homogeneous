8184139689e8415b7ea413d5ad12b9aa06188645
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 18:41:21 +0800

    Committed on or around 2024/03/09

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/MemberAccessInheritance/accessor functions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class TwoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	void showDim() {
+		std::cout << "Width and height are " << width << " and " << height << "\n";
+	}
+
+	// accessor functions
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() { return height; }
+	void setWidth(double w) { width = w; }
+	void setHeight(double h) { height = h; }
+
+};
+
+int main() {
+
+
+
+}
