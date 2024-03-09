6e30bab4cecbc20c127f45d778b86943f4faa439
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 16:36:01 +0800

    Committed on or around 2024/03/09

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance2/access is optional.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance2/access is optional.cpp
@@ -19,7 +19,7 @@ class Triangle : twoDShape {
 public:
 	char style[20];
 
-	double arcs() {
+	double area() {
 
 		// Triangle can refer to members of TwoDShape as if they were part of Triangle.
 		return width * height / 2;
@@ -29,3 +29,30 @@ public:
 		std::cout << "Triangle is " << style << "\n";
 	}
 };
+
+
+int main() {
+
+	Triangle t1;
+	Triangle t2;
+
+	// All members of Triangle are available to Triangle objects, even those inherited from TwoDShape.
+	t1.width = 4.0;
+	t1.height = 4.0;
+	strcpy(t1.style, "isosceles");
+
+	t2.width = 8.0;
+	t2.height = 12.0;
+	strcpy(t2.style, "right");
+
+	std::cout << "Info for t1: \n";
+	t1.showStyle();
+	t1.showDim();
+	std::cout << "Area is " << t1.area() << "\n";
+
+	std::cout << "\n";
+	std::cout << "Info for t2:\n";
+	t2.showStyle();
+	t2.showDim();
+	std::cout << "Area is " << t2.area() << "\n";
+}
