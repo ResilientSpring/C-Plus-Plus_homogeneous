f470b62a62f10af8868d80c4b92dc33411c51638
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-09 19:14:37 +0800

    Committed on or around 2024/03/09

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/MemberAccessInheritance/accessor functions.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/MemberAccessInheritance/accessor functions.cpp
@@ -41,4 +41,22 @@ int main() {
 	Triangle t1;
 	Triangle t2;
 
+	t1.setWidth(4.0);
+	t2.setHeight(4.0);
+	strcpy(t1.style, "isosceles");
+
+	t2.setWidth(8.0);
+	t2.setHeight(12.0);
+	strcpy(t2.style, "right");
+
+	cout << "Info for t1: \n";
+	t1.showStyle();
+	t1.showDim();
+	cout << "Area is " << t1.area() << endl;
+
+	cout << endl;
+	cout << "Info for t2: \n";
+	t2.showStyle();
+	t2.showDim();
+	cout << "Area is " << t2.area() << "\n";
 }
