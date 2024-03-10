4ba01453554f87fdb2b8e9f4d33e02cd5e744316
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-10 17:35:42 +0800

    Committed on or around 2024/03/10

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Using protected Members/Source.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Using protected Members/Source.cpp
@@ -19,6 +19,21 @@ public:
 	}
 };
 
+class D : public B {
+	int k;
+public:
+	// D may access B's a and b
+	void set_k() {
+		k = a * b;
+	}
+
+	void show_k() {
+
+		cout << k << "\n";
+
+	}
+};
+
 
 int main() {
 
