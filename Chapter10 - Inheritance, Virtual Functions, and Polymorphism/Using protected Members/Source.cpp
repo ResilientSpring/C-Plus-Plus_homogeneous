f4c926a2cb92fdb39afe51f2cc4e816db430a5ab
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-10 17:29:02 +0800

    Committed on or around 2024/03/10

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Using protected Members/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class B {
+
+protected:
+	int a, b;
+
+public:
+	void set(int a, int b) {
+		this->a = a;
+		this->b = b;
+	}
+
+};
+
+
+int main() {
+
+
+
+}
