578eb41253f377ea99defcae20ea7c46ba93d25d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-11 12:27:21 +0800

    Committed on or around 2024/03/11

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Call Base Class Constru/Calling Base Class Constructors.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class TwoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	TwoDShape(double width, double height) {
+		this->width = width;
+		this->height = height;
+	}
+
+	void showDim() {
+
+		cout << "width and height are " << width << " and " << height << endl;
+
+	}
+
+	// accessor functions 
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	void setWidght(double width) {
+		this->width = width;
+	}
+
+	void setHeight(double height) {
+		this->height = height;
+	}
+};
+
+class Triangle : public TwoDShape {
+	
+	char style[20];
+
+public:
+
+	Triangle(const char* str, double width, double height) :TwoDShape(width, height) {
+		strcpy(style, str);
+	}
+};
+
+
+int main() {
+
+
+
+}
