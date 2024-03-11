2e3634f116719e9a5b18a66602fcf7891ac73ffa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-11 13:25:34 +0800

    Committed on or around 2024/03/11

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Call Base Class Constr4/Any form of constructor defined by the base class can be called by the derived class' constructor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class TwoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	TwoDShape() {
+		width = height = 0.0;
+	}
+
+	TwoDShape(double width, double height) {
+
+		this->width = width;
+		this->height = height;
+	}
+};
+
+
+int main() {
+
+
+
+}
