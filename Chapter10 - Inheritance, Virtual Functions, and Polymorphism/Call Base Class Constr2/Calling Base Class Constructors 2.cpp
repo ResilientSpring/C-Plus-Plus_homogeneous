d6382c115d49e50b148b64a372a4e09f2097c90c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-11 12:43:02 +0800

    Committed on or around 2024/03/11

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Call Base Class Constr2/Calling Base Class Constructors 2.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class TwoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	TwoDShape(double width, double height) {
+		this->width = width;
+		this->height = height;
+	}
+
+	void showDim() {
+
+		cout << "width and height are " << width << " and " << height << endl;
+
+	}
+
+	// accessor functions 
+	double getWidth() {
+		return width;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	void setWidght(double width) {
+		this->width = width;
+	}
+
+	void setHeight(double height) {
+		this->height = height;
+	}
+};
+
+class Triangle : public TwoDShape {
+
+	char style[20];
+
+public:
+
+	Triangle(const char* str, double width, double height) :TwoDShape(w, h) {  // Call TwoDShape constructor.
+		strcpy(style, str);
+	}
+
+	double area() {
+		return getWidth();
+	}
+
+	void showStyle() {
+		cout << "Triangle is " << style << "\n";
+	}
+};
+
+
+int main() {
+
+	Triangle t1("isosceles", 4.0, 4.0);
+	Triangle t2("right", 8.0, 12.0);
+
+	cout << "Info for t1: \n";
+	t1.showStyle();
+	t1.showDim();
+	cout << "Area is " << t1.area() << endl;
+
+	cout << endl;
+	cout << "Info for t2: \n";
+	t2.showStyle();
+	t2.showDim();
+	cout << "Area is " << t2.area() << endl;
+
+}
