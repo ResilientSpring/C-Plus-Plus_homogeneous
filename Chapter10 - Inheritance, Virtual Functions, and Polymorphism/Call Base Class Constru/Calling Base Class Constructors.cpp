4a2fee042c89b6f285430972e7a61dba740705da
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-11 12:32:30 +0800

    Committed on or around 2024/03/11

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Call Base Class Constru/Calling Base Class Constructors.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Call Base Class Constru/Calling Base Class Constructors.cpp
@@ -59,6 +59,18 @@ public:
 
 int main() {
 
+	Triangle t1("isosceles", 4.0, 4.0);
+	Triangle t2("right", 8.0, 12.0);
 
+	cout << "Info for t1: \n";
+	t1.showStyle();
+	t1.showDim();
+	cout << "Area is " << t1.area() << endl; 
+
+	cout << endl;
+	cout << "Info for t2: \n";
+	t2.showStyle();
+	t2.showDim();
+	cout << "Area is " << t2.area() << endl;
 
 }
