7b43dda9fa988d3e703db714d49ffd4a3eda3432
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-16 23:45:21 +0800

    Committed on or around 2024/03/16

---
+++ Chapter 9 - A Closer Look at Classes/Friend_functions_5/Cylinder.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Cylinder; // a forward declaration.
+
+enum colors {red, green, yellow};
+
+class Cube {
+
+	colors color;
+
+public:
+
+	Cube(colors c) {
+		color = c;
+	}
+
+	friend bool sameColor(Cube x, Cylinder y);
+
+};
+
+class Cylinder {
+	colors color;
+
+public:
+	Cylinder(colors c) {
+		color = c;
+	}
+
+	friend bool sameColor(Cube x, Cylinder y);
+};
+
+bool sameColor(Cube x, Cylinder y) {
+	if (x.color == y.color) return true;
+	else return false;
+}
+
+int main() {
+
+
+
+}
