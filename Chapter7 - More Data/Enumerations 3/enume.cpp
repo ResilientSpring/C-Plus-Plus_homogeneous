3cdaefe95fffbe6ae0880c16219852707295b67e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-17 12:52:51 +0800

    Committed on or around 2024/03/17

--- Chapter7 - More Data/Enumerations 3/enume.cpp
+++ Chapter7 - More Data/Enumerations 3/enume.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 // Defined a list of named integer that represents 0, 1, 2, and so forth.
 enum transport
-{ 
-  // 0 ,   1  ,    2    , and so forth.
-	car, truck, airplane, train, boat
+{
+	// 0 ,   1  ,     10     ,  11  ,  12  .
+	car, truck, airplane = 10, train, boat
 };
 
 int main() {
@@ -14,6 +14,10 @@ int main() {
 
 	how = airplane;
 
-	cout << airplane << endl;  
+	cout << airplane << endl;
+
+	cout << train << endl;
+
+	cout << boat << endl;
 
 }
