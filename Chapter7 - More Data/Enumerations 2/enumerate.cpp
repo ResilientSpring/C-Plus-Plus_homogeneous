8fc011ea7b97f730c2f87bc451c0caba3588e4dc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-17 12:22:53 +0800

    Committed on or around 2024/03/17

---
+++ Chapter7 - More Data/Enumerations 2/enumerate.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Defined a list of named integer
+enum transport
+{
+	car, truck, airplane, train, boat
+};
+
+int main() {
+
+	transport how;
+
+	how = airplane;
+
+	cout << airplane << endl;
+
+}
