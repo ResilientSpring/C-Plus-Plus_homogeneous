c2ad5b26ba41c04c91ddf34f3962c13ff587da20
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-17 12:30:44 +0800

    Committed on or around 2024/03/17

--- Chapter7 - More Data/Enumerations 2/enumerate.cpp
+++ Chapter7 - More Data/Enumerations 2/enumerate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Defined a list of named integer
+// Defined a list of named integer that represents 0, 1, 2, and so forth.
 enum transport
 {
 	car, truck, airplane, train, boat
