6e843e6ccd370a2fb5a5e50547d68b14dd461929
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-17 13:06:39 +0800

    Committed on or around 2024/03/17

---
+++ Chapter7 - More Data/Enumerations 4/tedious.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+enum transport
+{
+	car, truck, airplane, train, boat
+};
+
+char name[][20] = {
+
+	"Automobile",
+	"Truck",
+	"Airplane",
+	"Train",
+	"Boat"
+
+};
+
+int main() {
+
+
+
+}
