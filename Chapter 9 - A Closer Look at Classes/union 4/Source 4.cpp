cfc31f9abe40479a75390040b25369477b038474
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-27 00:17:20 +0800

    Committed on or around 2024/03/27

---
+++ Chapter 9 - A Closer Look at Classes/union 4/Source 4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+union u_type {
+
+	short int i;
+	char ch[2];
+
+	// constructor
+	u_type(short int a) {
+		i = a;
+	}
+
+	// constructor
+	u_type(char x, char y) {
+
+		ch[0] = x;
+		ch[1] = y;
+
+	}
+
+	void showchars() {
+		cout << ch[0] << " ";
+		cout << ch[1] << "\n";
+	}
+
+};
+
+
+int main() {
+
+	u_type u(1000);
+
+	cout << "u as integer: " << u.i << "\n";
+	cout << "u as chars: ";
+	u.showchars();
+
+	u_type u2('X', 'Y');
+	cout << "u2 as integer: " << u2.i << "\n";
+	cout << "u2 as chars: ";
+	u2.showchars();
+}
+
