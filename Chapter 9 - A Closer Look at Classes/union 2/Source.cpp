e4c54276ece33019308a86c547e70180fa74960b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-27 00:05:20 +0800

    Committed on or around 2024/03/27

--- Chapter 9 - A Closer Look at Classes/union 2/Source.cpp
+++ Chapter 9 - A Closer Look at Classes/union 2/Source.cpp
@@ -31,15 +31,11 @@ int main() {
 
 	u_type u(1000);
 
-	cout << "u as integer: ";
-	cout << u.i << "\n";
-	cout << "u as chars: ";
-	u.showchars();
+	cout << "u as integer: " << u.i << "\n";
+	cout << "u as chars: " << u.showchars();
 
 	u_type u2('X', 'Y');
-	cout << "u2 as integer: ";
-	cout << u2.i << "\n";
-	cout << "u2 as chars: ";
-	u2.showchars();
+	cout << "u2 as integer: " << u2.i << "\n";
+	cout << "u2 as chars: " << u2.showchars();
 
 }
