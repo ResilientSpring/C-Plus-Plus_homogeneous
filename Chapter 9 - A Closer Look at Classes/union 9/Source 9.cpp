3c7965b5271e32e832876b5c40ebe6fe50215150
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-27 14:35:16 +0800

    Committed on or around 2024/03/27

---
+++ Chapter 9 - A Closer Look at Classes/union 9/Source 9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+union u_type {
+
+	short int i;
+	char ch[2];
+
+};
+
+
+int main() {
+
+	u_type u;
+	u.i = 27237;
+
+	// Figure 9-1 shows how i and ch both share the same address.
+	cout << "u as integer: " << u.i << "\n";
+	cout << "ch[0]: " << u.ch[0] << endl;
+	cout << "ch[1]: " << u.ch[1] << endl;
+}
+
