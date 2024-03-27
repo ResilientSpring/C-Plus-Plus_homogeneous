cd8f2688f70d020bd789359b0cc1e81b4ce52ce8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-27 13:02:44 +0800

    Committed on or around 2024/03/27

---
+++ Chapter 9 - A Closer Look at Classes/union 7/Source 7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+union u_type {
+
+	short int i;
+	char ch[2];
+
+};
+
+
+int main() {
+
+	u_type u;
+	u.i = 100;
+
+	cout << "u as integer: " << u.i << "\n";
+	cout << "u as chars: " << u.ch[0] << u.ch[1] << endl;
+
+}
+
