0ada144ade32b448daf1f8fb7dd413cf6f6058c3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 21:50:17 +0800

    Committed on or around 2024/04/19

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Create a Multilevel Hie/Hierarchical.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Create a Multilevel Hie/Hierarchical.cpp
@@ -23,6 +23,19 @@ public:
 		width = height = x;
 	}
 
+	void showDim() {
+
+		cout << "Width and height are " << width << " and " << height << "\n";
+
+	}
+
+	double getWidth() { return width; }
+	double getHeight() { return height; }
+	void setWidth(double w) { width = w; }
+	void setHeight(double h) {
+		height = h;
+	}
+
 };
 
 int main() {
