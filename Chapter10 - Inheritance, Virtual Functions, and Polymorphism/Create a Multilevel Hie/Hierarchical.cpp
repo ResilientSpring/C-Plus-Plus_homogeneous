4766eb9243b2bb7e52ea29e6704a6f8a7d521947
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 21:45:51 +0800

    Committed on or around 2024/04/19

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Create a Multilevel Hie/Hierarchical.cpp
@@ -0,0 +1,28 @@
+// A multilevel hierarchy.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class TwoDShape {
+
+	double width;
+	double height;
+
+public:
+
+	TwoDShape() {
+		width = height = 0.0;
+	}
+
+	TwoDShape(double w, double h) {
+		width = w;
+		height = h;
+	}
+
+};
+
+int main() {
+
+
+
+}
