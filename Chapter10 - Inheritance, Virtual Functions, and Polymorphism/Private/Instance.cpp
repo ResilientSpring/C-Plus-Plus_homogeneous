1a6ef4f75807486bdda8a3c00f3c0a4e3db37f23
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 14:14:41 +0800

    Committed on or around 2024/04/24

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Private/Instance.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Private/Instance.cpp
@@ -11,14 +11,19 @@ public:
 	void showDim() {
 		std::cout << "Width and height are " << width << " and " << height << "\n";
 	}
-
 };
 
+class Shape {
+	double width, height;
+public:
+	void set(double width, double height) {
+		this->width = width;
+		this->height = height;
+	}
+};
 
 int main() {
-
 	twoDShape t1;
-
-	t1.height;
-	t1.width;
+	t1.height = 2;
+	t1.width = 3;
 }
