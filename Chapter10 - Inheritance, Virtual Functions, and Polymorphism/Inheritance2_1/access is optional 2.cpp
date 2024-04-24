c10459ac49b9e3c15fac5f33448d60f93630c25a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 13:58:01 +0800

    Committed on or around 2024/04/24

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance2_1/access is optional 2.cpp
@@ -0,0 +1,57 @@
+// A simple class hierarchy
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+class twoDShape {
+public:
+	double width;
+	double height;
+
+	void showDim() {
+		std::cout << "Width and height are " << width << " and " << height << "\n";
+	}
+
+};
+
+// If the access specifier is not used, then it is private by default if the derived class is a class.
+class Triangle : twoDShape {
+public:
+	char style[20];
+
+	double area() {
+
+		// Triangle can refer to members of TwoDShape as if they were part of Triangle.
+		return width * height / 2;
+	}
+
+	void showStyle() {
+		std::cout << "Triangle is " << style << "\n";
+	}
+};
+
+
+int main() {
+
+	Triangle t1;
+	Triangle t2;
+
+	t1.width = 4.0;
+	t1.height = 4.0;
+	strcpy(t1.style, "isosceles");
+
+	t2.width = 8.0;
+	t2.height = 12.0;
+	strcpy(t2.style, "right");
+
+	std::cout << "Info for t1: \n";
+	t1.showStyle();
+	t1.showDim();
+	std::cout << "Area is " << t1.area() << "\n";
+
+	std::cout << "\n";
+	std::cout << "Info for t2:\n";
+	t2.showStyle();
+	t2.showDim();
+	std::cout << "Area is " << t2.area() << "\n";
+}
