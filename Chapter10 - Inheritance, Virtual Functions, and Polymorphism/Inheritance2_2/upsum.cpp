690a505f8c11a48546315f5fca4c8eea07b19bb0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 14:53:39 +0800

    Committed on or around 2024/04/24

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance2_2/upsum.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Inheritance2_2/upsum.cpp
@@ -2,16 +2,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
+using namespace std;
 
 class twoDShape {
+	int number;
 public:
-	double width;
-	double height;
+	double width, height;
 
 	void showDim() {
-		std::cout << "Width and height are " << width << " and " << height << "\n";
+		cout << "Width and height are " << width << " and " << height << "\n";
 	}
-
 };
 
 // If the access specifier is not used, then it is private by default if the derived class is a class.
@@ -22,11 +22,11 @@ public:
 	double area() {
 
 		// Triangle can refer to members of TwoDShape as if they were part of Triangle.
-		std::cout << "Area is " << width * height / 2 << "\n";
+		cout << width * height / 2 << "\n";
 	}
 
 	void showStyle() {
-		std::cout << "Triangle is " << style << "\n";
+		cout << "Triangle is " << style << "\n";
 	}
 };
 
@@ -34,26 +34,21 @@ public:
 int main() {
 
 	Triangle t1;
-	Triangle t2;
 
 	// When the base class is inherited as private, 
 	// then all public members of the base class become private members of the derived class.
 	t1.width = 4.0;
 	t1.height = 4.0;
-	strcpy(t1.style, "isosceles");  
-
-	t2.width = 8.0;
-	t2.height = 12.0;
-	strcpy(t2.style, "right");  // The style is accessible.
+	strcpy(t1.style, "isosceles");  // The style is accessible.
 
 	std::cout << "Info for t1: \n";
 	t1.showStyle();
 	t1.showDim();  // Class twoDshape's public showDim() has become a private member of class Triangle.
-	t1.area();
+	t1.area() ;
 
 	std::cout << "\n";
 	std::cout << "Info for t2:\n";
 	t2.showStyle();
 	t2.showDim();
-    t2.area();  // area() is accessible.
+	std::cout << "Area is " << t2.area() << "\n";  // area() is accessible.
 }
