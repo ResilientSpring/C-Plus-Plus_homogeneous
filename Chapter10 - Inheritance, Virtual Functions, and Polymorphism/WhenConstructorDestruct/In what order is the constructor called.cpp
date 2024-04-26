eb07fa8eab7544ad004b9100fbc9133bfac0122c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-26 13:00:50 +0800

    Committed on or around 2024/04/26

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/WhenConstructorDestruct/In what order is the constructor called.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class B {
+
+public:
+	B() {
+		cout << "Constructing base portion\n";
+	}
+
+	~B() {
+		cout << "Destructing base portion\n";
+	}
+
+};
+
+int main() {
+
+
+
+}
