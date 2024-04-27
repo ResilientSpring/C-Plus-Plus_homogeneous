4c5be9048aa2e539923ce277f06d750995348d4d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-27 21:08:26 +0800

    Committed on or around 2024/04/27

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Pointer to Derived Type/polymorphism.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class B {
+
+public:
+	B() {
+		cout << "Constructing base portion\n";
+	}
+
+	~B() {
+		cout << "Destructing base portion\n";
+	}
+
+};
+
+class D : public B {
+
+public:
+	D() {
+		cout << "Constructing derived portion\n";
+	}
+
+	~D() {
+		cout << "Destructing derived portion\n";
+	}
+
+};
+
+int main() {
+
+	D ob;
+
+	B* p;  // pointer to object of type B
+	B B_ob; // object of type B
+	D D_ob;  // object of type D
+}
