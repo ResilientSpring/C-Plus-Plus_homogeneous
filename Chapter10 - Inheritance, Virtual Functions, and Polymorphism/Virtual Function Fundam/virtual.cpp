8230f67251f8164c2c5063d76c399a715e53d27d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-28 23:32:00 +0800

    Committed on or around 2024/04/28

---
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Virtual Function Fundam/virtual.cpp
@@ -0,0 +1,23 @@
+// A short example that uses a virtual function.
+
+#include <iostream>
+using namespace std;
+
+class B {
+
+public:
+
+	// Declare a virtual function.
+	virtual void who() {  
+		cout << "Base\n";
+	}
+
+};
+
+
+int main() {
+
+
+
+
+}
