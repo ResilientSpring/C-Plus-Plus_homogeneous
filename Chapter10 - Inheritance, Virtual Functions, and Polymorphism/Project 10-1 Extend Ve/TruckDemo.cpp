b7db50927d3e9bb128681256d35e81f2c824902f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-01 23:22:51 +0800

    Committed on or around 2024/05/01

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Project 10-1 Extend Ve/TruckDemo.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Project 10-1 Extend Ve/TruckDemo.cpp
@@ -60,5 +60,5 @@ int main() {
 
 	cout << "It has a range of " << semi.range() << " miles.\n";
 
-	cout << "To go " << dist << " miles semi needs " << dist / pickup.
+	cout << "To go " << dist << " miles, semi needs " << dist / semi.get_mpg() << " gallons of fuel.\n\n";
 }
