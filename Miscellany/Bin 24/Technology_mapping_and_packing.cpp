b2c39bbe6024935efd231dbe8f30992089f26a0e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-05 12:43:00 +0800

    Committed on or around 2024/05/05

--- Miscellany/Bin 24/Technology_mapping_and_packing.cpp
+++ Miscellany/Bin 24/Technology_mapping_and_packing.cpp
@@ -114,6 +114,11 @@ int main(int argc, char **argv) {
 	// character encoding. [25]
 	K = stoi(argv[2]);
 
+	cout << "Input File: " << argv[1] << endl;
+	cout << "K: " << argv[2] << endl;
+	cout << "Output File: " << argv[3] << endl;
+	cout << "CLB's input size constraint: " << argv[4] << endl;
+
 	stack<int> gates;
 
 	Topological_sort(gates);
