c4991debce5bb76ce7b2f04ff01e5188fb118e17
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-23 00:13:12 +0800

    Committed on or around 2024/05/23

---
+++ Assortment/Job bank 4-7/macro- 4.cpp
@@ -0,0 +1,16 @@
+#define ADD(x) x*x
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int // a = 4,
+		// b = 6,
+		// c = 7,
+		// d = ADD(a + b) * c;
+		d = ADD(4 + 6) * 7;
+
+	printf("d = %d", d);
+
+}
