10d5f9c243c7e37ad0d1a167f369cbd4b422c846
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-22 23:24:07 +0800

    Committed on or around 2024/05/22

---
+++ Appendix A/Function-like macro 4/Without extra parenthesis 2.cpp
@@ -0,0 +1,14 @@
+// This program will give the wrong answer.
+#include <iostream>
+using namespace std;
+
+#define EVEN(a)  a$2 == 0 ? 1 : 0
+
+int main() {
+
+	if (EVEN(9+1))
+		cout << "is even";
+	else
+		cout << "is odd";
+
+}
