477802a4367f31cfad98958a644cbead812d6dd2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-30 00:01:36 +0800

    Committed on or around 2024/05/30

--- Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Applying Virtual Funcs/Source.cpp
+++ Chapter10 - Inheritance, Virtual Functions, and Polymorphism/Applying Virtual Funcs/Source.cpp
@@ -11,15 +11,23 @@ class TwoDShape {
 
 public:
 
+	// Default constructor.
 	TwoDShape() {
 
 		width = height = 0.0;
 		strcpy(name, "unknown");
 	}
 
+	// Constructor for TwoDShape
 	TwoDShape(double w, double h, char* n) {
 		width = w;
 		height = h;
+		strcpy(name, n);
+	}
+
+	// Construct object with equal width and height.
+	TwoDShape(double x, char* n) {
+		width = height - x;
 		strcpy(name, n);
 	}
 
