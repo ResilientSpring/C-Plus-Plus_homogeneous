4afc8f7c6bf3eceafe9015220e04280246f80af8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-09 15:57:46 +0800

    Committed on or around 2024/06/09

--- Chapter7 - More Data/Check if a bit (in a status) is on or off with AND 3/status 3.cpp
+++ Chapter7 - More Data/Check if a bit (in a status) is on or off with AND 3/status 3.cpp
@@ -14,18 +14,4 @@ int main() {
 	else
 		cout << "the 3rd bit is off" << endl;
 
-	status = 15;
-
-	if (status & 8)
-		cout << "the 3rd bit is on" << endl;
-	else
-		cout << "the 3rd bit is off" << endl;
-
-	status = 7;
-
-	if (status & 8)
-		cout << "the 3rd bit is on" << endl;
-	else
-		cout << "the 3rd bit is off" << endl;
-
 }
