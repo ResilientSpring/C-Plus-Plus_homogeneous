7609819098cfbc5b53b596a85b0b33976a952040
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-09 18:59:15 +0800

    Committed on or around 2024/06/09

--- Chapter7 - More Data/show_binary()_/lullaby.cpp
+++ Chapter7 - More Data/show_binary()_/lullaby.cpp
@@ -1,11 +1,9 @@
-// #include <iostream>
-#include "insofar_as_.cpp"
-using namespace std;
-
-// extern void show_binary(unsigned int u);
+extern void show_binary(unsigned int u);
 
 int main() {
 
 	show_binary(9);
 	show_binary(8);
+
+	show_binary(255);
 }
