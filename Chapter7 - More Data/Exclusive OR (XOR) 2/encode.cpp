f72a10d48729bb6d6bb460f6f8aaa2a89ad2ada8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-13 00:01:11 +0800

    Committed on or around 2024/06/13

--- Chapter7 - More Data/Exclusive OR (XOR) 2/encode.cpp
+++ Chapter7 - More Data/Exclusive OR (XOR) 2/encode.cpp
@@ -11,15 +11,15 @@ int main() {
 
 	// Exclusive OR sets a bit on only when the bits being compared are different.
 
+	// 	XOR has an interesting property making it a way to encode an array of characters.
 	for (int i = 0; i < strlen(msg); i++)
-		msg[i] = msg[i] ^ key;   
+		msg[i] = msg[i] ^ key;    // When A is XORed with B, and C ensues.
 
 	cout << "Encoded message: " << msg << endl;
 
-	// XOR has an interesting property making it a way to encode an array of characters.
 
 	for (int i = 0; i < strlen(msg); i++)
-		msg[i] = msg[i] ^ key;  
+		msg[i] = msg[i] ^ key;   // When C is 
 
 	cout << "Decoded message: " << msg << "\n";
 
