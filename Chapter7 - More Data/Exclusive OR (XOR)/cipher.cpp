94d67c0781960d404cc136f29dc5277a5cfc42e1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-12 20:18:27 +0800

    Committed on or around 2024/06/12

--- Chapter7 - More Data/Exclusive OR (XOR)/cipher.cpp
+++ Chapter7 - More Data/Exclusive OR (XOR)/cipher.cpp
@@ -5,6 +5,14 @@ int main() {
 
 	char msg[] = "This is a test";
 
+	char key = 88;
+
 	cout << "Original message: " << msg << "\n";
+
+
+	for (int i = 0; i < strlen(msg); i++)
+	{
+		msg[i] = msg[i] ^ key;    // This construts the encoded string.
+	}
 
 }
