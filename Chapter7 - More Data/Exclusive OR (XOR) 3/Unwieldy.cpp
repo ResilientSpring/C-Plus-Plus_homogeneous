f3bbe12c09cd603d381642c281632ef4cb034431
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-15 23:57:17 +0800

    Committed on or around 2024/06/15

--- Chapter7 - More Data/Exclusive OR (XOR) 3/Unwieldy.cpp
+++ Chapter7 - More Data/Exclusive OR (XOR) 3/Unwieldy.cpp
@@ -14,6 +14,14 @@ int main() {
 	// 	XOR has an interesting property making it a way to encode an array of characters.
 	for (int i = 0; i < strlen(msg); i++)
 		msg[i] = msg[i] ^ key;    // When A is XORed with B, C ensues.
+	       //  0101 0100 ^ 0101 1000
+
+	       //  0101 0100
+	       //  0101 1000
+	//   ^  -------------------
+	//         0000 1100
+
+
 
 	cout << "Encoded message: " << msg << endl;
 
