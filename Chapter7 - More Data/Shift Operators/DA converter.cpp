fa40fb5364d0913bcf8029bf64a4c9198b7b4fdf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-20 13:20:44 +0800

    Committed on or around 2024/06/20

--- Chapter7 - More Data/Shift Operators/DA converter.cpp
+++ Chapter7 - More Data/Shift Operators/DA converter.cpp
@@ -12,6 +12,11 @@ int main() {
 
 void show_binary(unsigned int decimal_number) {
 
+	for (int i = 128; i > 0; i = i / 2)
+		if (decimal_number & i)
+			cout << "1 ";
+		else
+			cout << "0 ";
 
-
+	cout << "\n";
 }
