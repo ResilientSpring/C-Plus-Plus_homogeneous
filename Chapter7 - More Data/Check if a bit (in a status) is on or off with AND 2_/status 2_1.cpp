8be630b76a52e2672a541a06892bc8a11b416b18
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-25 15:40:22 +0800

    Verifying the statement on page 257 that "if x equals 7, then x && 8 evaluates to true, whereas x & 8 evaluates to false."

---
+++ Chapter7 - More Data/Check if a bit (in a status) is on or off with AND 2_/status 2_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int status;
+
+	status = 8;
+
+	if (status & 8)
+		cout << "bit 4 is on" << endl;
+	else
+		cout << "bit 4 is off" << endl;
+
+	status = 15;
+
+	if (status & 8)
+		cout << "bit 4 is on" << endl;
+	else
+		cout << "bit 4 is off" << endl;
+
+	status = 7;
+
+	if (status & 8)
+		cout << "bit 4 is on" << endl;
+	else
+		cout << "bit 4 is off" << endl;
+
+	if (x && 8)
+		cout << "When status = 7, (x && 8) evaluates to true " << endl;
+
+}
