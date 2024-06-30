bda3fe6c5b31e7866eadf169feefd8875141c658
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-30 17:41:26 +0800

    removed the redundant space.

--- Assortment/Swap characters/Proctor.cpp
+++ Assortment/Swap characters/Proctor.cpp
@@ -6,7 +6,7 @@ int main() {
 	char a[] = "err on the side of caution.";
 	char b[] = "wishful thinking";
 
-	printf("The content of the 'a' array of characters: % c \n", a);
+	printf("The content of the 'a' array of characters: %c \n", a);
 
 
 }
