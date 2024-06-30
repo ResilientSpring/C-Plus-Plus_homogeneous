97bf33ddbdbad4d13323ea18109666874e90ef5f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-30 17:40:47 +0800

    Attempting to exchange characters in two variables without using an intermediate variable.

---
+++ Assortment/Swap characters/Proctor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char a[] = "err on the side of caution.";
+	char b[] = "wishful thinking";
+
+	printf("The content of the 'a' array of characters: % c \n", a);
+
+
+}
