afc004fefb78bf1dbc17e86c151f6cc3fd5c48ea
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-07-08 23:58:31 +0800

    Committed on or around 2024/07/08

---
+++ Chapter7 - More Data/Project 7-1 Create Bitwise Rotation Functions/rotate.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Left-rotate a byte n places.
+unsigned char lrotate(unsigned char val, int n) {
+
+	unsigned int t;
+
+	t = val;
+
+	for (int i = 0; i < n; i++)
+	{
+		t = t << 1;
+
+		/*  If a bit shifts out, it will be in bit 8 of the integer t. 
+		   
+		    If this is the case, put that bit on the right side.
+		
+		
+		*/
+
+	}
+
+}
+
+
+int main() {
+
+
+
+}
