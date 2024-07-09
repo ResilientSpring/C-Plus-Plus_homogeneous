df0e9b6566e31a2363cc307bd70adb1cccb1b255
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-07-09 18:03:36 +0800

    Try to use short division to procure the binary representation of a decimal number.

---
+++ Assortment/Short division/tow.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void short_division_of_an_unsigned_integer_by_2(unsigned int u);
+
+int main() {
+
+
+
+}
+
+
+void short_division_of_an_unsigned_integer_by_2(unsigned int u) {
+
+	if (u % 2 == 1) {
+		printf("1 ");
+		u /= 2;
+		short_division_of_an_unsigned_integer_by_2(u);
+	}
+	else if (u % 2 == 0) {
+		printf("0 ");
+		u /= 2;
+		short_division_of_an_unsigned_integer_by_2(u);
+	}
+	else if (u < 1) {
+
+	}
+}
