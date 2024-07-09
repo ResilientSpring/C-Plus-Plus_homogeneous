1d43188af2843a7b4d83c12d39d8369bda6be81a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-07-09 22:12:58 +0800

    Iterative version of the short division algorithm

---
+++ Assortment/Short division 5/tow 5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void short_division_of_an_unsigned_integer_by_2(unsigned int u);
+
+int main() {
+
+	short_division_of_an_unsigned_integer_by_2(192);
+
+}
+
+
+void short_division_of_an_unsigned_integer_by_2(unsigned int u) {
+
+	for (int i = u; i > 0; i /= 2) {
+
+		int remainder = i % 2;
+
+		switch (remainder)
+		{
+		case '1':
+			printf("1");
+			break;
+		case '0':
+			printf("0");
+			break;
+		}
+	}
+
+}
